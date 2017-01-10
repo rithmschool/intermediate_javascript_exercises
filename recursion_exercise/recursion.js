59aa21b85c0b435b140141be94b21b120f0a8af8
Matt Lane <lane.matthew@gmail.com>
2017-01-10 10:49:02 -0800

    indentation

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -76,15 +76,15 @@ function binarySearch(array, key, left=0, right=array.length) {
 }
 
 function stringifyNumbers(obj) {
-	var newObj = {};
-	for (var key in obj) {
-		if (typeof obj[key] === 'number') {
-           newObj[key] = obj[key].toString();
-        } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-            newObj[key] = stringifyNumbers(obj[key]);
-        } else {
-            newObj[key] = obj[key];
-        }
-	}
-	return newObj;
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
 }
