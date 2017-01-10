61dcc7726d46d7dcd3c5958d6663424ffe54ce8d
Matt Lane <lane.matthew@gmail.com>
2017-01-10 10:47:37 -0800

    update stringifyNumbers solution

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -75,16 +75,16 @@ function binarySearch(array, key, left=0, right=array.length) {
   }
 }
 
-function stringifyNumbers(obj){
-    var newObj = {};
-    for(var key in obj){
-        if(typeof obj[key] === 'number'){
-            newObj[key] = String(obj[key])
-        } else if(typeof obj[key] === 'object' && !Array.isArray(obj[key])){
-            newObj[key] = Object.assign(Object.create(Object.prototype,{newObj}), stringifyNumbers(obj[key]))
+function stringifyNumbers(obj) {
+	var newObj = {};
+	for (var key in obj) {
+		if (typeof obj[key] === 'number') {
+           newObj[key] = obj[key].toString();
+        } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+            newObj[key] = stringifyNumbers(obj[key]);
         } else {
-            newObj[key] = obj[key]
+            newObj[key] = obj[key];
         }
-    }
-    return newObj;
+	}
+	return newObj;
 }
