7590b2c15b755bca9a6e182290d2bb265e5d69c4
Andrew Mundy <andrewmundy@gmail.com>
2017-01-19 13:48:15 -0800

    three

--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -1,29 +1,55 @@
-function drop(){
-
+function drop(arr,n){
+    if(n === undefined){n=1}
+    return arr.slice(n);
 }
 
-function fromPairs(){
-
+function fromPairs(arr){
+    var obj = {};
+    arr.forEach(function(x){
+       obj[x[0]] = x[1]; 
+    });
+    
+    return obj;
 }
 
-function head(){
-
+function head(arr){
+	return arr[0];
 }
 
-function take(){
-
+function take(arr,n){
+    if(n === undefined){
+        return arr.slice(0,1)
+    }else{
+        return arr.slice(0,n)
+    }
 }
 
-function takeRight(){
-
+function takeRight(arr,n){
+    if(n === undefined){
+        return arr.slice(arr.length-1);
+    }else if(n<arr.length){
+        return arr.slice(arr.length-n);
+    }else {
+        return arr
+    }
 }
 
-function takeWhile(){
-
+function takeWhile(arr,callback){
+    return arr.filter(function(x){
+        return callback(x);
+    })
 }
 
-function union(){
+function union(arr,add){
+    var newArr = arr.concat(add)
+    var finalArr = [];
 
+    newArr.forEach(function(val){
+        if(!finalArr.includes(val)){
+            finalArr.push(val)
+        }
+    })
+    return finalArr;
 }
 
 function zip(){
