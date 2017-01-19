516f4f40ee7768f5ac24bc11c1653d53c5641448
Aaron Manley <AaronManley@Aarons-MacBook-Pro.local>
2017-01-19 13:47:31 -0800

    up to zip

--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -1,38 +1,92 @@
-function drop(){
-
+function drop(arr, n) {
+    if (n === undefined) {n = 1};
+    return arr.slice(n);
 }
 
-function fromPairs(){
+function fromPairs(pairs) { 
+    obj = {};
+    pairs.forEach(function (val) {
+        obj[val[0]] = val[1];
+    });
+    return obj;
+};
 
+function head(arr){
+	return arr[0];
 }
 
-function head(){
-
+function take(arr, n) {
+    if (n === undefined) {n = 1};
+    return arr.slice(0, n);
 }
 
-function take(){
-
+function takeRight(arr, n) {
+    if (n === undefined) {n = 1};
+    if (n === 0) {return []}
+    return arr.slice(-n);
 }
 
-function takeRight(){
+function takeWhile (arr, callback) {
+    return arr.filter(callback)
+};
 
+
+function union (arrays) {
+    var args = Array.from(arguments);
+    uniq = [];
+    args.forEach(function(x) {
+        x.forEach(function(y) {
+            if (!uniq.includes(y)) {
+                uniq.push(y);
+            }
+        })
+    })
+return uniq;
 }
 
-function takeWhile(){
+function zip (arr) {
+    var arr = Array.from(arguments);
+    var ans = [];
+    var leng = 3;
+    arr.forEach(function (x) {
+        if (x.length < leng) {
+            leng = x.length;
+        }
+    })
+    arr.forEach(function (val1, idx1) {
+        val1.forEach(function (val2, idx2) {
+            if (ans[idx2] === undefined) {
+                ans[idx2] = [];
+            } 
+            ans[idx2].push(val2);
+        })
+    });
+    for (i = 0; i < ans.length; i++) {
+        while (leng >= ans[i].length) {
+            ans[i].push(undefined);
+        }
+    }
+    return ans;
+};
 
-}
 
-function union(){
+function unzip (arr) {
+    //loop through intial set of arrays
+    //at each input, send the 1st,2nd,3rd,nth element to 
+    //corr ans arr.
 
-}
+var ansArr = [];
+var args = Array.from(arguments);
+args.forEach(function (val, idx) {
+    val.forEach(function (val2, idx2) {
+        if (ansArr[idx2] === undefined) {
+            ansArr[idx2] = [];
+        }
+        ansArr[idx2].push(val2);
+    })
+})
+return ansArr;
 
-function zip(){
-
-}
-
-function unzip(){
-
-}
 
 function zipObject(){
 
