d742f420c3118d1a567a792dbfd103e93ee946b2
tostaylo <tostaylo1@gmail.com>
2017-01-18 18:25:02 -0800

    lodash libray to includes - zip

--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -1,45 +1,122 @@
-function drop(){
 
+function drop(arr, index){
+  if(index === undefined){
+      return arr.slice(1);
+  } else {
+      return arr.slice(index);
+  }
 }
 
-function fromPairs(){
 
+function fromPairs(arr){
+    var newObj = {};
+    arr.forEach(function(item){
+        newObj[item[0]] = item[1];
+    })
+    return newObj;
 }
 
-function head(){
 
+
+function head(arr){
+    return arr[0];
 }
 
-function take(){
 
+function take(arr, index){
+    if(index === undefined){
+        return arr.slice(0,1);
+    } else {
+        return arr.slice(0, index);
+    }
 }
 
-function takeRight(){
+function takeRight(arr, index){
+    
+    if(index === undefined){
+        return arr.slice(arr.length -1);
+    }  else if(index > arr.length){
+        return arr.slice();
+    } else  {
+        return arr.slice(arr.length - index);
+    }
+        }
 
+
+
+function takeWhile(arr, callback){
+    var newArr = [];
+    arr.forEach(function(item){
+        if(callback(item)){
+            newArr.push(item);
+        }
+    })
+    return newArr;
 }
 
-function takeWhile(){
-
+function union(arr1, arr2){
+    var newArr = arr1.concat(arr2);
+    var lastArr = [];
+    newArr.forEach(function(item){
+      if(lastArr.indexOf(item) === -1){
+          lastArr.push(item);
+      }
+    })
+    return lastArr;
 }
 
-function union(){
 
-}
-
-function zip(){
-
-}
+function zip(arr1, arr2, arr3){
+   
+ 
+ }
 
 function unzip(){
 
 }
 
-function zipObject(){
 
+   function zipObject(arr1,arr2){
+    var obj={};
+    for(var i = 0; i < arr1.length; i++){
+        obj[arr1[i]] = arr2[i];
+    }
+
+    return obj;
 }
 
-function includes(){
 
+function includes(collection, target, startIndex){
+    if(typeof(collection) === 'string'){
+    if(collection.indexOf(target) !== -1){
+        return true;
+    } else {
+        return false;
+    }
+   }
+   if(Array.isArray(collection)){
+       if(startIndex !== undefined){
+           var slicedArr = collection.slice(startIndex);
+           if(slicedArr.indexOf(target) !== -1){
+           return true;
+       } else{
+           return false;
+       }
+
+       }else if(collection.indexOf(target) !== -1){
+           return true;
+       } else{
+           return false;
+       }
+   }
+
+   if(typeof(collection) === "object"){
+       for(var key in collection){
+           if(collection[key] === target){
+               return true;
+           } 
+       }
+   }
 }
 
 function sample(){
