735f5681bf6ca0d8ba2e68a2bafa65338efa0a1c
tostaylo <tostaylo1@gmail.com>
2017-01-23 14:04:49 -0800

    working on favorites functionality

--- jquery_exercise/main.js
+++ jquery_exercise/main.js
@@ -1,12 +1,34 @@
 $(document).ready(function () {
     let $title;
+    let $url;
     //Add List Item
     $('button').click(function (e) {
         e.preventDefault();
         $title = $('#title-input').val();
-        $('#links-ul').append('<li>' + $title + '</li>');
+        $url = $('#url-input').val();
+        $('#links-ol').append('<li> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>' + $title + $url + '</li>');
         $($title).val(" ");
     });
 
     //Show Form On click
+    $('#submit').click(function () {
+        $('form').show();
+        $(this).hide();
+    })
+
+    //Toggle Star Functionality
+    $('body').on('click', '.glyphicon', function () {
+        $(this).toggleClass('glyphicon-star-empty glyphicon-star');
+    });
+
+    //Show Favorites
+    $('#favorites').click(function () {
+
+        $("li").each(function (index) {
+            if ($("span").hasClass("glyphicon-star") === true) {
+
+                $('.glyphicon-star-empty').parent().hide();
+            }
+        });
+    })
 })
