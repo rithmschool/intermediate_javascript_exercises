4f72210923cc8bcf7d8f1ebe6fd2d9a065c046f2
tostaylo <tostaylo1@gmail.com>
2017-01-23 15:56:37 -0800

    added domain to li, working on filtering functionality
    echo

--- jquery_exercise/main.js
+++ jquery_exercise/main.js
@@ -6,9 +6,32 @@ $(document).ready(function () {
         e.preventDefault();
         $title = $('#title-input').val();
         $url = $('#url-input').val();
-        $('#links-ol').append('<li> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>' + $title + $url + '</li>');
-        $($title).val(" ");
+
+        //Validate URL and Add Item to List
+        if ($url.indexOf('www') !== -1 && $url.indexOf('.com') !== -1) {
+
+            let $domainIndex = $url.indexOf('.');
+            let $domain = $url.slice($domainIndex + 1);
+
+
+            $('#links-ol').append('<li> <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span><a href =' + $url + '>' + $title + '</a><span class="domain-span">(' + $domain + ')</span></li>');
+
+
+            $('#title-input').val("");
+            $('#url-input').val("");
+            $('#error').hide();
+
+            //Show Domain Name in Li
+
+
+        } else {
+            $('#error').show();
+        }
+
+
     });
+
+
 
     //Show Form On click
     $('#submit').click(function () {
@@ -26,9 +49,37 @@ $(document).ready(function () {
 
         $("li").each(function (index) {
             if ($("span").hasClass("glyphicon-star") === true) {
-
                 $('.glyphicon-star-empty').parent().hide();
+                $('#favorites').hide();
+                $('#all').show();
             }
         });
     })
+
+    //Show All
+    $('#all').click(function () {
+
+        $("li").each(function (index) {
+            if ($("span").hasClass("glyphicon-star") === true) {
+                $('.glyphicon-star-empty').parent().show();
+                $('#favorites').show();
+                $('#all').hide();
+            }
+        });
+    })
+
+    //Show only Items of clicked domain
+    $('body').on('click', '.domain-span', function () {
+        console.log('clicked');
+        let $domainName = $(this).text();
+        console.log($domainName);
+        $(".domain-name").each(function (index) {
+            if (index.text() === $domainName) {
+                console.log('we have a match');
+            }
+        });
+
+    })
+
+
 })
