d80aefd748737fd9ca24bd1801b468d84b01351e
Aaron Manley <AaronManley@Aarons-MacBook-Pro.local>
2017-01-25 18:04:49 -0800

    completed excercises

--- call_apply_bind_exercise/callApplyBind.js
+++ call_apply_bind_exercise/callApplyBind.js
@@ -1,5 +1,7 @@
+
 function sumEvenArguments () {
-	var args = Array.from(arguments);
+// var args = Array.from(arguments);
+	var args = [].slice.call(arguments);
 	var ans = 0;
 	args.forEach(function (x) {
 		if (x % 2 === 0) {
@@ -26,3 +28,26 @@ function invokeMax (fn, num) {
 		}
 	}
 };
+
+function guessingGame(num) {
+    var count = 0;
+    return function(guess) {
+        count += 1;
+        if (count === num) {
+            return "You are all done playing fool.";
+        }
+        var ans = Math.round(Math.random()*10.1);
+        console.log(ans);
+        if (guess === ans) {
+            return "You won! Imagine that..."
+        } else {
+            return "You are wrong, and are dumb."
+        }
+    }
+}
+
+
+// guessingGame(5)
+// var game1 = guessingGame(2);
+
+var game1 = guessingGame(4);
