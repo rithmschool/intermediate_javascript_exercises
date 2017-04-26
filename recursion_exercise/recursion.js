06429b2d6d033dec604e1e1f2e0feba3c3265e85
Julia Hazer <juliahazer@yahoo.com>
2017-04-25 21:49:18 -0700

    finished bonus exercises in recursion.js

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -190,4 +190,20 @@ function stringifyNumbers(obj){
 Complete 
 https://www.codewars.com/kata/mutual-recursion/train/javascript) codewars problem! 
 */
+/*CodeWars account name: juliahazer */
+function F(n) { 
+  if (n === 0) {
+    return 1;
+  }
+  return n - M(F(n-1));
+}
+
+function M(n) { 
+  if (n === 0) {
+    return 0;
+  }
+  return n - F(M(n-1));
+}
+
+
 
