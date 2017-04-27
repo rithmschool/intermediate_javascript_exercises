3d228db27329b99c5f1b536b2f929174a140659e
Dan Lennon <safarileader@gmail.com>
2017-04-27 09:17:59 -0700

    exercises (not bonus) nearly complete. Am vexxed by has() and probalby need to make it recursive.

--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -1,29 +1,50 @@
-function drop(){
-
+function drop(arr, param){
+	if (param === undefined) return arr.slice(1);
+	if (param > arr.length) return [];
+	if (param === 0 ) return arr;
+	return arr.slice(param);
 }
 
-function fromPairs(){
-
+function fromPairs(arr){
+	var returnObj = {};
+	while (arr.length>0){
+		var shiftArr = arr.shift();
+		returnObj[shiftArr[0]] = shiftArr[1]
+	}
+	return returnObj
 }
 
-function head(){
-
+function head(arr){
+	return arr[0];
 }
 
-function take(){
-
+function take(arr, param){
+	if (param === 0) return [];
+	if (param === undefined) return [arr[0]];
+	if (param >= arr.length) return arr;
+	return arr.slice(0,param);
 }
 
-function takeRight(){
-
+function takeRight(arr, param){
+	if (param === undefined) return [arr[arr.length-1]];
+	if (param >= arr.length) return arr;
+	if (param === 0) return [];
+	return arr.slice(arr.length-param);
 }
 
 function takeWhile(){
 
 }
 
-function union(){
-
+function union(arr1,arr2){
+    var unionArr = [];
+    for (var i =0; i< arr1.length; i++){
+        if (! unionArr.includes(arr1[i])) unionArr.push(arr1[i]);
+    }
+    for (var i =0; i< arr2.length; i++){
+        if (! unionArr.includes(arr2[i])) unionArr.push(arr2[i]);
+    }
+    return unionArr;
 }
 
 function zip(){
@@ -34,68 +55,200 @@ function unzip(){
 
 }
 
-function zipObject(){
+function zipObject(keyArr, valArr){
+	var returnObj = {};
+	while (keyArr.length > 0){
+		returnObj[keyArr.shift()] = valArr.shift() || undefined;
+	}
+	return returnObj;
+}
+
+function includes(arr, target, startIndex){
+	startIndex = startIndex || 0;
+	// ('abcd', 'bc') true
+	if (typeof arr === 'string'){
+		for (var i =startIndex; i<= arr.length-target.length; i++){
+			if(arr.slice(i,target.length+i) === target){
+				return true;
+			} 
+		}
+		return false;
+	}
+	console.log(arr);
+	if (typeof arr === 'object'){
+		var keyArr = Object.keys(arr);
+		var resultArr = [];
+		for (var i=0; i<keyArr.length; i++){
+            resultArr.push(arr[keyArr[i]])
+		}
+		arr = resultArr;
+	}
+
+
+	for (var i = startIndex; i < arr.length; i++){
+		if (arr[i] === target) return true;
+	}
+	return false;
 
 }
 
-function includes(){
-
-}
-
-function sample(){
-
+function sample(arr){
+	return arr[Math.floor(Math.random()*arr.length)];
 }
 
 function flip(){
 
 }
 
-function cloneDeep(){
-
+function cloneDeep(arr){
+	return {};
 }
 
-function sumBy(){
-
+function sumBy(objects, callback){
+    if (typeof callback === 'function'){
+    	return objects.reduce(function(acc, cur, idx, array){
+    		acc += callback(cur);
+    		// function(o) { return o.n; }
+    		return acc;
+    	},0)
+    } else {
+        // let's hope it's keys :)
+        return objects.reduce(function(acc, cur, idx, array){
+    		acc += cur[callback];
+    		// function(o) { return o.n; }
+    		return acc;
+    	},0)
+    }
 }
 
-function inRange(){
+function inRange(num, start, end){
+    if (end < 0){
+        var temp = end;
+        end = start;
+        start = temp;
+    }
+	if ( ! end) {
+		end = start;
+		start = 0;
+	}
 
+	return start < num && num < end;
 }
 
-function has(){
+function has(obj, targetKey){
 
+    // obj - we're looking for keys
+    // targetKey = "value" cool, let's look.
+    // if Array.isArray(targetKey), let's look for an array of values
+    // which array method was the "all of them?"
+
+    return targetKey.every(function(val, idx, array){
+        if (obj[val]){
+            return true;
+        } else {
+            return false;
+        }
+    })
 }
 
-function omit(){
+function omit(obj, arr){
+  keysArr = Object.keys(obj);
+  resultObj = {};
 
+  for (var i = 0; i < keysArr.length; i++){
+     if ( ! arr.includes(keysArr[i])) {
+        resultObj[keysArr[i]] = obj[keysArr[i]];
+     }
+   }
+
+   return resultObj;
 }
 
-function pick(){
+function pick(obj, arr){
+  keysArr = Object.keys(obj);
+  resultObj = {};
 
+  for (var i = 0; i < keysArr.length; i++){
+     if (arr.includes(keysArr[i])) {
+        resultObj[keysArr[i]] = obj[keysArr[i]];
+     }
+   }
+   
+   return resultObj;
 }
 
-function pickBy(){
+function pickBy(obj, callback){
+  keysArr = Object.keys(obj);
+  resultObj = {};
 
+  for (var i = 0; i < keysArr.length; i++){
+     if (callback(obj[keysArr[i]]) ) {
+        resultObj[keysArr[i]] = obj[keysArr[i]];
+     }
+   }
+   
+   return resultObj;
 }
 
-function omitBy(){
+function omitBy(obj, callback){
+  keysArr = Object.keys(obj);
+  resultObj = {};
 
+  for (var i = 0; i < keysArr.length; i++){
+     if (! callback(obj[keysArr[i]]) ) {
+        resultObj[keysArr[i]] = obj[keysArr[i]];
+     }
+   }
+   
+   return resultObj;
 }
 
-function pad(){
+function padEnd(str, len, padString){
+	padString = padString || "  ";
+	padCopy = padString;
+	padString = padString.split("");
+	padCopy = padCopy.split(""); // copies of arrays create references!
 
+	if (str.length === len) { return str;}
+
+	stringArray = str.split("");
+
+	while (stringArray.length < len){
+		if (padString.length == 0) padString = padString.concat(padCopy)
+		stringArray.push(padString.shift());
+	}
+
+	stringArray = stringArray.join("");
+	return stringArray;
 }
 
-function repeat(){
-
+function repeat(str, num){
+	if (num === 0) return "";
+	if (num > 0){
+		return str + repeat(str, num-1);
+	} 
 }
 
 function upperFirst(str){
-
+	str = str.split("");
+	str[0] = str[0].toUpperCase();
+	str = str.join("");
+	return str;
 }
 
-function flatten(){
-
+function flatten(arr){
+    var returnArr = []
+    for (var i = 0 ; i < arr.length ; i++){
+        if (! Array.isArray(arr[i])){
+         returnArr.push(arr[i]);
+        } else{
+           // Now we have an array to deal with
+            arr[i].forEach(function(val){
+                returnArr.push(val);
+             })         
+        }
+    }
+    return returnArr;
 }
 
 function flattenDeep(){
