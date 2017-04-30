c28cdd2823d5353a458c809a1818bdd76e0f707c
V. Lai <lai.vivian@gmail.com>
2017-04-30 13:12:18 -0700

    update recursion.js with bonus questions

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -93,7 +93,7 @@ function replicate(times, number) {
 }
 
 /* BONUS */
-function search(arr, val) { 
+function search(arr, val) {
     var index = 0;
 
     function searchHelper(a, v) {
@@ -114,7 +114,45 @@ function search(arr, val) {
 // search()- can it be done without a helper function? - try it later!
 
 // binary search
+function binarySearch(arr, val, start, end) {
+    var start = start || 0;
+    var end = end || arr.length - 1;
+    var mid = Math.floor((start + end) / 2);
+    if (val === arr[mid]) return mid; // found correct
+    // don't forget about the edge cases of val less than start or val greater than start
+    if (start > end || val < arr[start] || val > arr[end]) return -1; // not in array!
+    if (val > arr[mid]) { // cut left
+        start = mid + 1;
+    }
+    if (val < arr[mid]) { // cut right
+        end = mid - 1;
+    }
+    return binarySearch(arr, val, start, end);
+}
 
-// stringify numbers
+// stringify numbers - no helper function needed!
+function stringifyNumbers(object) {
+    var newObj = {};
+    for (var key in object) {
+        if (typeof object[key] === "object" && !Array.isArray(object[key])) {
+            newObj[key] = stringifyNumbers(object[key]);
+        } else if (typeof object[key] === "number") { // if value is a number
+            newObj[key] = String(object[key]);
+        } else {  // array, strings, and booleans
+            newObj[key] = object[key];
+        }
+    }
+    return newObj;
+}
 
 // codewars problem
+// https://www.codewars.com/kata/mutual-recursion/train/javascript
+function F(n) {
+    if (n < 1) return 1;
+    return n - M(F(n - 1));
+}
+
+function M(n) {
+    if (n < 1) return 0;
+    return n - F(M(n - 1));
+}
