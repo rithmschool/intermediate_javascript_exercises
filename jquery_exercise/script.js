399507124ae8ac3829010dd69ce06711077e1d12
Shriya K Nevatia <sneva831@gmail.com>
2017-05-01 16:46:29 -0700

    Revert "if url begins with www. we hide that part in the url displayed next to the title"
    This reverts commit 91e4040dc7b5775ad8a77dd07c9ba37285ebd77b.

--- jquery_exercise/script.js
+++ jquery_exercise/script.js
@@ -1,21 +1,24 @@
+// FIX THIS VALIDATION
+// just for the demos, avoids form submit
+jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+});
+$("form").validate({
+    rules: {
+        field: {
+            required: true,
+            url: true
+        }
+    }
+});
+
+
 $(document).ready(function () {
     // toggling submit form (WRONG)
     $("#submission").on("click", function () {
         $("#submit").toggle();
     });
-
-    jQuery.validator.setDefaults({
-    debug: true,
-    success: "valid"
-	});
-	$("form").validate({
-	    rules: {
-	        field: {
-	            required: true,
-	            url: true
-	        }
-	    }
-	});
 
     // append new item to form
     // create a new item
@@ -23,15 +26,11 @@ $(document).ready(function () {
         e.preventDefault();
         var $postTitle = $("#post-title").val();
         var $postUrl = $("#post-url").val();
-        var $partialUrl = $postUrl.match(/\w+\.\w+\.\w+/); // url after http:// and before path; something.something.something
-        
-        if ($partialUrl[0].match(/^www/)) { // if www. start, remove "www."
-        	$partialUrl[0] = $partialUrl[0].substring(4);
-        }
+        var $partialUrl = $portUrl
 
         var $mainList = $("#main ol");
-        if (($postUrl.includes('http://') || $postUrl.includes('https://')) && (/\w+\.+\w/).test($postUrl)) {
-            var html = '<li><span class="star"></span><span class="link-title"><a href="' + $postUrl + '">' + $postTitle + '</a></span> <span class="link-url">(' + $partialUrl + ')</span></li>';
+        if (($postUrl.includes('http://') || $postUrl.includes('https://')) && (/\w+\./).test($postUrl)) {
+            var html = '<li><span class="star"></span><span class="link-title"><a href="' + $postUrl + '">' + $postTitle + '</a></span> <span class="link-url">(' + $postUrl + ')</span></li>';
             $mainList.append(html);
         }
     });
