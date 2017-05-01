6599309a0a8182a47a427abf10b631513542682b
Tobi Ogunnaike <oluwatobi.ogunnaike@gmail.com>
2017-05-01 00:25:50 -0700

    Ajax call returning undefined because I'm not correctly accesing the url from the response object...I can't see my mistake atm

---
+++ Giphy_party/script.js
@@ -0,0 +1,34 @@
+$(function(){
+	$("#searchButton").on("click", function(event){
+		let $searchText = $("#searchText").text();   // collect the user's input. store in jquery variable
+		let $gifHolder = $(".gifHolder")  			//retrieves our holder div where we will keep all our images
+		
+		// make an AJAX request to the GIPHY API using our user's input
+		// create a HTML img element --> set it's src === url returned by ajax call 
+		// append all images to our $gifHolder div
+		// use css method to set display:flexbox and set height/width of images
+
+		$.ajax({
+			url: "http://api.giphy.com/v1/gifs/search?q=" + $searchText + "&api_key=dc6zaTOxFJmzC",
+			type: "GET",
+			success: function(response){
+				$('<img />').attr('src', response.data[0].url).appendTo($gifHolder); // attr() expects strings not objects ???
+			},
+			error: function(error){
+        		console.log(error);
+    		}
+		})
+	})
+
+	$("#deleteButton").on("click", function(event){
+		let $gifHolder = $(".gifHolder");
+		$gifHolder.empty();  //remove all child elements nested in $gifHolder div
+
+	})
+})
+
+// Aims: 
+//Allow the user to search for a GIF and when the form is submitted, make an AJAX request to the Giphy API and return a single GIF
+// Once the Giphy API has responded with data, append the GIF to the page
+// Allow the user to search for as many GIFs as they would like and keep appending them to the page
+// Allow the user to remove all of the GIFs by clicking a button
