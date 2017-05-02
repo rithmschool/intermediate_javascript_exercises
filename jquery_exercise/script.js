1edbfd7ef68a5ccf124d688fab52fe8435e10964
V. Lai <lai.vivian@gmail.com>
2017-05-01 17:15:34 -0700

    update jquery hack or snooze w/ more functionality - worked with Shriya

--- jquery_exercise/script.js
+++ jquery_exercise/script.js
@@ -13,17 +13,23 @@ $("form").validate({
     }
 });
 
-
 $(document).ready(function () {
-    // the DOM has now loaded
-    // $(function () {
-    //     $('#submit').hide();
-    // });
-
     // toggling submit form (WRONG)
-    $("#submission").on("click", function () {
-        $('#submit').removeClass('hidden');
+    $("#submission").on("click", function (e) {
+        e.preventDefault();
         $("#submit").toggle();
+    });
+
+    var $ol = $("ol");
+    $ol.on("click", ".fa", function (e) {
+        e.preventDefault();
+        if ($(e.target).hasClass('fa-star-o')) {
+            $(e.target).removeClass('fa-star-o');  //missing . before removeClass
+            $(e.target).addClass('fa-star');
+        } else {
+            $(e.target).removeClass('fa-star');
+            $(e.target).addClass('fa-star-o');
+        }
     });
 
     // append new item to form
@@ -32,11 +38,18 @@ $(document).ready(function () {
         e.preventDefault();
         var $postTitle = $("#post-title").val();
         var $postUrl = $("#post-url").val();
+        var $partialUrl = $postUrl.match(/\w+\.\w+\.\w+/); // url after http:// and before path; something.something.something
+
+        if ($partialUrl[0].match(/^www/)) { // if www. start, remove "www."
+            $partialUrl[0] = $partialUrl[0].substring(4);
+        }
 
         var $mainList = $("#main ol");
         if (($postUrl.includes('http://') || $postUrl.includes('https://')) && (/\w+\./).test($postUrl)) {
-            var html = '<li><span class="star"></span><span class="link-title"><a href="' + $postUrl + '">' + $postTitle + '</a></span> <span class="link-url">(' + $postUrl + ')</span></li>';
+            var html = '<li><span class="fa fa-star-o"></span><span class="link-title"><a href="' + $postUrl + '">' + $postTitle + '</a></span> <span class="link-url">(' + $partialUrl + ')</span></li>';
             $mainList.append(html);
         }
     });
+
+
 });
