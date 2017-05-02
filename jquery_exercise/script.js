3dc16f644a8f73303fa38fd55c04298bf1dd1487
Dan Lennon <safarileader@gmail.com>
2017-05-01 17:55:10 -0700

    bonus features activated

--- jquery_exercise/script.js
+++ jquery_exercise/script.js
@@ -23,25 +23,47 @@ $(function() {
 	})
 
 	$form= $("form");
+
 	$form.on('submit',function(e){
 		console.log(e);
 
 		e.preventDefault();
 		// create new div. as a new last child to 
-		$newDiv = $("<div>");
-		$newSpan = $("<span>");
-		$newA = $("<a>");
-		
-		$newDiv.text($("#theList>div").length+1 + ". "); // make this dynamic.
+		$newSpan = $("<span>")
+			.attr('class', "glyphicon glyphicon-star-empty");
 
-		$newSpan.attr('class', "glyphicon glyphicon-star-empty");
-		$newA.attr('href', $("#url").val());
-		$newA.attr('target', '_blank');
-		$newA.text($("#site").val())
+		$newNumSpan = $("<span>")
+			.addClass("number")
+			.text($("#theList>div").length+1 + ". ");
+
+		$newA = $("<a>", {
+			href: $("#url").val(),
+			target: "_blank",
+			text: $("#site").val()
+		});
+
+		let url = $newA.attr('href');
+		url = url.split("://")[1].split("/")[0];
+		if (url.split(".").length > 2){
+			url = url.split(".").slice(-2).join(".");
+		}
+		// extract domain:
+
+		$newUrlSpan = $("<span>")
+			.attr('data-domain', url)
+			.text("(" + url + ")");
+		
+		$newUrlSmall = $("<small>")
+			.append($newUrlSpan);
+
+
+		$newDiv = $("<div>")
+			.append($newNumSpan)
+			.append($newSpan)
+			.append($newA)
+			.append($newUrlSmall)
 
 		$listSection.append($newDiv);
-		$newDiv.append($newSpan);
-		$newDiv.append($newA);
 
 		$form.trigger("reset");
 
@@ -50,9 +72,9 @@ $(function() {
 
 	})
 
-	$icons = $(".glyphicon");
-
-	$icons.on('click',function(e){
+	
+	$icons = $("#theList");
+	$icons.on('click',".glyphicon",function(e){
 		if ($(e.target).hasClass("glyphicon-star-empty")){
 			$(e.target).removeClass("glyphicon-star-empty");
 			$(e.target).addClass("glyphicon-star");
@@ -61,24 +83,55 @@ $(function() {
 			$(e.target).removeClass("glyphicon-star");
 			$(e.target).addClass("glyphicon-star-empty");
 		}
-	});
+	})
 
 	let $faves = $("#favorites");
 	let favesToggle = false;
 
-	$faves.on('click',function(e){
 
+	$icons.on('click',"div span:last-child",function(e){
+		//let site = $(e.target).parent().parent().children().eq(2).text();//.split("://")[1].split("/")[0];
+		let site = $(e.target).parent().parent().children().eq(3).children().eq(0).attr('data-domain');
+		//$(`#theList span:last-child:not([data-domain='${site}'])`).parent().parent().css('display','none');
+		debugger;
+		$("#theList span:last-child:not([data-domain='" + site + "'])").parent().parent().css('display','none');
+		$faves.text("All");
+
+		let $numbers = $(".number");
+		$numbers.css('display', 'none');
+
+		favesToggle=true;
+		// favorites disappear;
+		// favorites becomes all
+	});
+
+
+
+
+
+	$faves.on('click',function(e){
 		if (favesToggle === false){
 			favesToggle = true;
 			let $hide = $("span.glyphicon-star-empty");
 			$hide.parent().css('display', 'none');
+			let $numbers = $(".number");
+			$numbers.css('display', 'none');
+			$faves.text("All");
 		} else {
 			favesToggle = false;
-			let $hide = $("span.glyphicon-star-empty");
-			$hide.parent().css('display', '');
+			//let $hide = $("span.glyphicon-star-empty");
+			//$hide.parent().css('display', '');
+			let $numbers = $(".number");
+			$numbers.css('display', '');
+			$faves.text("Favorites");
+
+			//unhide any hidden divs.
+			$("#theList").children().css('display','');
+
 		}
 
 	})
 
 
 });
+
