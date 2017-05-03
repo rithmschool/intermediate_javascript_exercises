4e80f9a9246f38af27a05ab643bf3bb2a69e53db
Julia Hazer <juliahazer@yahoo.com>
2017-05-02 22:13:44 -0700

    added functionality to switch between favorites/all in hack or snooze app

--- jquery_exercise/script.js
+++ jquery_exercise/script.js
@@ -4,59 +4,23 @@ $(function(){
 	var $loginForm = $('#loginForm');
 	var $signupForm = $('#signupForm');
 	var $favLink = $('#favLink');
+	var $favSpan = $('#favSpan');
 	var $ol = $('#resultList ol')
 
 	//whether logged in or not
 	var loggedIn = false;
 	//favorites selected
 	var favoritesOnly = false;
-	//one domain only selected
-	var oneDomainOnly = false;
 
-	var storiesArr = [];
 	var numStories = 20;
 
 	$forms.hide();
+	$favSpan.hide();
 
+	//to begin app
 	hackerNewsApiCalls();
 
 	/*********************FUNCTIONS*********************/
-
-	function loginSignup(urlText, email, password){
-		$.ajax({
-			url: `https://hn-favorites.herokuapp.com/${urlText}`,
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				email: email, 
-				password: password 
-			}
-		}).then(function(data){
-			localStorage.setItem('auth_token', data.auth_token);
-			// console.log("localStorage: " + localStorage.getItem('auth_token'));
-
-			/*FIX THIS!!!!!*/
-			return $.ajax({
-				url: 'https://hn-favorites.herokuapp.com/stories.json',
-				dataType: 'json',
-				headers: {
-					Authorization: localStorage.auth_token
-				}
-			})
-		}).then(function(data){
-			console.log(data);
-			$forms.slideToggle();
-			setAsLoggedIn();
-		}).catch(function(data){
-			alert("Login Error! Please try again");
-		});
-	}
-
-	function setAsLoggedIn(){
-		loggedIn = true;
-		$('.star').show();
-		$loginLink.text('logout');
-	}
 
 	function hackerNewsApiCalls(){
 		/*
@@ -73,8 +37,9 @@ $(function(){
 			});
 			return Promise.all(promiseArr).then(function(res){
 				// console.log(res);
+				$ol.html('');
 				res.forEach(function(storyObj){
-					writeLiHtml(storyObj);
+					writeLiHtml(storyObj, favoritesOnly);
 				});
 				//check if logged in
 				if(localStorage.getItem('auth_token')){
@@ -84,22 +49,75 @@ $(function(){
 		})
 	}
 
+	function getFavorites(){
+		$.ajax({
+			url: 'https://hn-favorites.herokuapp.com/stories.json',
+			dataType: 'json',
+			headers: {
+				Authorization: localStorage.auth_token
+			}
+		}).then(function(data){
+			console.log(data);
+			$ol.html('');
+			favoritesOnly = true;
+			data.forEach(function(storyObj){
+				writeLiHtml(storyObj, favoritesOnly);
+			});
+			$favLink.text('all');
+		});
+	}
+
 	/*writes the lis with story info to the list*/
-	function writeLiHtml(storyObj){
+	function writeLiHtml(storyObj, isFavList){
 		var domainUrl = extractDomain(storyObj.url);
-		var timestamp = new Date(storyObj.time*1000);
-		var now = new Date().getTime();
-		var timeAgo = timeDifference(now, timestamp);
+		/*
+			for Hacker News api, time is in unix format
+			and stored in .time property
+			display time 'ago' posted
+		*/
+		if (!isFavList){
+			var timestamp = new Date(storyObj.time*1000);
+			var now = new Date().getTime();
+			var timeAgo = timeDifference(now, timestamp);
+		}
+		/*
+			for favorite list for heroku api,
+			time is stored in .created_at property
+			diplay time favorited
+		*/
+		else {
+			var timeAdded = new Date(storyObj.created_at);
+			var month = timeAdded.getMonth() + 1;
+			var day = timeAdded.getDay();
+			var year = timeAdded.getFullYear();
+		}
 		var liHtml = `
 			<li 
-				data-by="${storyObj.by}"
-				data-id="${storyObj.id}"
+				data-by="${storyObj.by}" 
+				data-id="`;
+		if (!isFavList){
+			liHtml += storyObj.id;
+		}
+		else {
+			liHtml += storyObj.story_id;
+		}
+		liHtml +=`"
 				data-title="${storyObj.title}"
-				data-url="${storyObj.url}"
-			>
+				data-url="${storyObj.url}" `;
+		if (isFavList) {
+			liHtml += `data-ajaxId="${storyObj.id}"`;
+		}
+		liHtml += `>
 				<span>
-					<span class="star glyphicon glyphicon-star-empty" 
-					aria-hidden="true"></span> 
+					<span class="star glyphicon `;
+		if (!isFavList){
+			liHtml += `glyphicon-star-empty`;
+		}
+		//display the filled in star for favorite list
+		else {
+			liHtml += `glyphicon-star`;
+		}
+		liHtml += `" aria-hidden="true"></span> 
 				</span> 
 				<span class='name'>
 					<a href='${storyObj.url}' target='_blank'>
@@ -110,14 +128,56 @@ $(function(){
 						(${domainUrl})
 				</span><br>
 				<span class="postInfo">
-					by ${storyObj.by} ${timeAgo}
+					by ${storyObj.by}`; 
+		if (!isFavList){
+			liHtml += " " + timeAgo;
+		}
+		else {
+			liHtml += `; Favorited: ${month}/${day}/${year}`;
+		}
+		liHtml += `
 				</span>
 			</li>
 		`;
 		$ol.append(liHtml);
 
-		//hide favorite star icons
+		//hide favorite star icons if not logged in
+		if (!loggedIn){
+			$('.star').hide();
+		}
+	}
+
+	function loginSignup(urlText, email, password){
+		$.ajax({
+			url: `https://hn-favorites.herokuapp.com/${urlText}`,
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				email: email, 
+				password: password 
+			}
+		}).then(function(data){
+			localStorage.setItem('auth_token', data.auth_token);
+			$forms.slideToggle();
+			setAsLoggedIn();
+		}).catch(function(data){
+			alert("Login Error! Please try again");
+		});
+	}
+
+	function setAsLoggedIn(){
+		loggedIn = true;
+		$('.star').show();
+		$favSpan.show();
+		$loginLink.text('logout');
+	}
+
+	function setAsLoggedOut(){
+		loggedIn = false;
 		$('.star').hide();
+		$favSpan.hide();
+		$loginLink.text('login');
+		localStorage.removeItem('auth_token');
 	}
 
 	/*
@@ -175,45 +235,17 @@ $(function(){
 
 	/*********************EVENT LISTENERS*********************/
 
-	/*when click "submit" w/in the LOGIN form*/
-	$loginForm.on('submit', function(e){
-		e.preventDefault();
-		var loginLogin = $('#loginLogin').val();
-		var loginPassword = $('#loginPassword').val();
-
-		loginSignup('login', loginLogin, loginPassword);
-		// console.log(`${loginLogin} and ${loginPassword}`);
-
-		$('#loginLogin').val('');
-		$('#loginPassword').val('');
-	});
-
-	/*when click "submit" w/in the SIGNUP form*/
-	$signupForm.on('submit', function(e){
-		e.preventDefault();
-		var signupLogin = $('#signupLogin').val();
-		var signupPassword = $('#signupPassword').val();
-
-		loginSignup('signup', signupLogin, signupPassword);
-		// console.log(`${signupLogin} and ${signupPassword}`);
-
-		$('#signupLogin').val('');
-		$('#signupPassword').val('');
-	});
-
-
-	/*hide and show login/signup forms when click "login" link in nav*/
+	/*
+	hide and show login/signup forms when 
+	click "login" link in nav
+	*/
 	$loginLink.on('click', function(e){
 		e.preventDefault();
 		//if logged in, LOGOUT!
 		if (loggedIn){
-			/*****HOW ELSE DO I LOG OUT???*********/
-			loggedIn = false;
-			$loginLink.text('login');
-			localStorage.removeItem('auth_token');
-			$('.star').hide();
-
+			setAsLoggedOut();
 		}
+		//if logged out and click, toggle login forms
 		else {
 			$forms.slideToggle();
 		}
@@ -223,31 +255,24 @@ $(function(){
 	$favLink.on('click', function(e){
 		e.preventDefault();
 
-		/*hide and show favorites when click "favorites/all" link in nav*/
-		if(!oneDomainOnly){
-			favoritesOnly = !favoritesOnly;
-			if (favoritesOnly){
-				$ol.find('.glyphicon-star-empty').parent().parent().hide();
-				$ol.css('list-style-type', 'none');
-				$(e.target).text('all');
-			}
-			else {
-				$ol.find('.glyphicon-star-empty').parent().parent().show();
-				$ol.css('list-style-type', 'decimal');
-				$(e.target).text('favorites');
-			}
+		/*if click the favorites link, then get favs*/
+		if ($favLink.text() === "favorites"){
+			getFavorites();
+			$ol.css('list-style-type', 'none');
 		}
-
-		/*show all in list (if currently only showing a specific domain)*/
-		else { 
-			oneDomainOnly = false;
-			$('li').show();
+		/*if click the all link, then get all top stories*/
+		else {
+			favoritesOnly = false;
+			$favLink.text('favorites');
+			hackerNewsApiCalls();
 			$ol.css('list-style-type', 'decimal');
-			$(e.target).text('favorites');
 		}
 	});
 
-	/*Add & remove favorites when click "star" w/in each li*/
+	/*
+	Add & remove favorites when click "star" w/in each li
+	Only for logged-in users
+	*/
 	$ol.on('click', '.star', function(e){
 		//first, toggle appearance of glyphicon star from empty to filled
 		//or vice versa
@@ -266,13 +291,14 @@ $(function(){
 					Authorization: localStorage.auth_token
 				}
 			}).then(function(res){
-				console.log(res);
+				if (favoritesOnly){
+					$li.hide();
+				}
 				$li.removeAttr('data-ajaxId');
 			});
 		}
 		//ADD FAVORITE
 		else {
-			console.log('add favorite');
 			var $li = $(e.target).parent().parent();
 			var by = $li.attr('data-by');
 			var id = $li.attr('data-id');
@@ -295,24 +321,33 @@ $(function(){
 					}
 				})
 			}).then(function(res){
-				console.log(res);
+				// console.log(res);
 				$li.attr('data-ajaxId', res.id);
 			});
 		}
 	});
 
-	/*when click on url, only show items with same url 
-	& switch the favorites link in nav bar to all*/
-	/*
-	$ol.on('click', '.url', function(e){
-		var activeUrlText = $(e.target).text().trim();
-		$('li').filter(function(i, el){
-			return $(el).find('.url').text().trim() !== activeUrlText;
-		}).hide();
-		// $('li:not(:contains' + activeUrlText + ')').hide();
-		oneDomainOnly = true;
-		$favLink.text("all");
-		$ol.css('list-style-type', 'none');
+	/*when click "submit" w/in the LOGIN form*/
+	$loginForm.on('submit', function(e){
+		e.preventDefault();
+		var loginLogin = $('#loginLogin').val();
+		var loginPassword = $('#loginPassword').val();
+
+		loginSignup('login', loginLogin, loginPassword);
+
+		$('#loginLogin').val('');
+		$('#loginPassword').val('');
 	});
-	*/
+
+	/*when click "submit" w/in the SIGNUP form*/
+	$signupForm.on('submit', function(e){
+		e.preventDefault();
+		var signupLogin = $('#signupLogin').val();
+		var signupPassword = $('#signupPassword').val();
+
+		loginSignup('signup', signupLogin, signupPassword);
+
+		$('#signupLogin').val('');
+		$('#signupPassword').val('');
+	});
 });
