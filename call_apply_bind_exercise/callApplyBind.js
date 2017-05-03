ae65e7a2746ac47075f6ec4d59d0a83dc130a7d2
Albert Kam <albertkam91@gmail.com>
2017-05-03 16:35:18 -0700

    added guessing game solution to callApplyBind.js

--- call_apply_bind_exercise/callApplyBind.js
+++ call_apply_bind_exercise/callApplyBind.js
@@ -28,3 +28,24 @@ function invokeMax(fn, num){
 		}
 	}
 }
+
+function guessingGame(num){
+	var counter = 0
+	var numGuess = num;
+	var number = Math.floor(Math.random() * 10);
+	return function(guess){
+		if(counter < numGuess){
+			counter++;
+			if(guess < number){
+				console.log("You're too low!")
+			} else if( guess > number){
+				console.log("You're too high!")
+			} else {
+				console.log("You got it!")
+				counter = numGuess + 1;
+			}
+		} else {
+			console.log("You are done playing!");
+		}
+	}
+}
