c46a71f7a63c06d808f1c241832fe7027a517f5b
V. Lai <lai.vivian@gmail.com>
2017-05-05 08:45:01 -0700

    update H/S clone with fav functionality. need to work on delete

--- ajax_with_jquery_exercise/script.js
+++ ajax_with_jquery_exercise/script.js
@@ -1,3 +1,5 @@
+// var topStories = [];
+// var myFavs = [];
 $(function () {
     // checking if logged in
     var storage = localStorage.getItem("auth_token") || "";
@@ -16,7 +18,7 @@ $(function () {
             $(".login").text("login/signup");
             localStorage.clear();
             $("aside").toggle();
-        }    
+        }
     });
 
     // login functionality
@@ -62,19 +64,23 @@ $(function () {
                 $.get(`https://hacker-news.firebaseio.com/v0/item/${res[i]}.json?print=pretty`)
                     .then(function (res) {
                         //console.log(res);
-                        var $partialUrl = (res.url.split(/\/\//)[1]).split(/\//)[0];
 
-                        if ($partialUrl.match(/^www/)) { // if www. start, remove "www."
-                            $partialUrl = $partialUrl.substring(4);
-                        }
-
-                        var $star = $("<span>").addClass("fa fa-star-o");
+                        var $resBy = $("<span>")
+                            .append(res.by)
+                            .toggle(false);
+                        var $star = $("<span>")
+                            .addClass("fa fa-star-o");
                         var $link = $("<a/>")
                             .attr("href", res.url)
                             .text(" " + res.title + " ");
                         var $linkTitle = $("<span>")
                             .addClass("link-title")
                             .append($link);
+                        var $partialUrl = (res.url.split(/\/\//)[1]).split(/\//)[0];
+
+                        if ($partialUrl.match(/^www/)) { // if www. start, remove "www."
+                            $partialUrl = $partialUrl.substring(4);
+                        }
                         var $pLink = $("<a/>")
                             .attr("href", "https://" + $partialUrl)
                             .text("(" + $partialUrl + ")");
@@ -82,10 +88,14 @@ $(function () {
                             .addClass("link-url")
                             .append($pLink);
                         var $newLi = $("<li>")
+                            .addClass(res.id.toString())
                             .append($star)
                             .append($linkTitle)
-                            .append($linkUrl);
+                            .append($linkUrl)
+                            .append($resBy);
                         $ol.append($newLi);
+                        //var article = res;
+                        //topStories.push(article);
                     });
             }
         });
@@ -96,17 +106,87 @@ $(function () {
         if ($(".login").text() === "logout") { // make sure logged in
             $(e.target).toggleClass('fa-star-o');
             $(e.target).toggleClass('fa-star');
+            $.ajax({
+                method: "POST",
+                url: "https://hn-favorites.herokuapp.com/stories.json",
+                contentType: "application/json",
+                dataType: "json",
+                headers: {
+                    Authorization: storage,
+                },
+                data: JSON.stringify({
+                    hacker_news_story: {
+                        by: $(e.target).next().next().next().text(),
+                        story_id: $(e.target).parent().attr('class'),
+                        title: $(e.target).next().text().slice(1, -1),
+                        url: $(e.target).next().children().attr('href')
+                    }
+                })
+            }).then(function (res) {
+                console.log(res);
+                // what next? -> saving the data?
+                //myFavs.push(res); 
+            }).catch(function (res) {
+                console.log("a mistake was made in POST fav");
+            });
         }
     });
 
     $(".favs").on("click", function (e) {
         e.preventDefault();
-        if ($(".favs").text() === "all" && $(".login").text() === "logout") {
+        if ($(".favs").text() === "all" && $(".login").text() === "logout") { // showing all
             $(".favs").text("favorites");
             $(".fa-star-o").parent().toggle(true);
-        } else if ($(".login").text() === "logout") {
+        } else if ($(".login").text() === "logout") { // showing favs
             $(".favs").text("all");
             $(".fa-star-o").parent().toggle(false);
+            $.ajax({
+                method: "GET",
+                url: "https://hn-favorites.herokuapp.com/stories.json",
+                headers: {
+                    Authorization: storage,
+                }
+            }).then(function (res) {
+                console.log(res); // an array of objects
+                // $ol.empty(); // NO?s
+                for (var key in res){
+                    // console.log(res[key].by);
+                    // console.log(res[key].story_id);
+                    // console.log(res[key].title);
+                    // console.log(res[key].url);
+                     var $resBy = $("<span>")
+                            .append(res[key].by)
+                            .toggle(false);
+                        var $star = $("<span>")
+                            .addClass("fa fa-star");
+                        var $link = $("<a/>")
+                            .attr("href", res[key].url)
+                            .text(" " + res[key].title + " ");
+                        var $linkTitle = $("<span>")
+                            .addClass("link-title")
+                            .append($link);
+                        var $partialUrl = (res[key].url.split(/\/\//)[1]).split(/\//)[0];
+
+                        if ($partialUrl.match(/^www/)) { // if www. start, remove "www."
+                            $partialUrl = $partialUrl.substring(4);
+                        }
+                        var $pLink = $("<a/>")
+                            .attr("href", "https://" + $partialUrl)
+                            .text("(" + $partialUrl + ")");
+                        var $linkUrl = $("<span>")
+                            .addClass("link-url")
+                            .append($pLink);
+                        var $newLi = $("<li>")
+                            .addClass(res[key].id.toString())
+                            .append($star)
+                            .append($linkTitle)
+                            .append($linkUrl)
+                            .append($resBy);
+                        $ol.append($newLi);
+                }
+            }).catch(function (res) {
+                console.log("a mistake was made in GET fav");
+            });
         }
     });
 });
