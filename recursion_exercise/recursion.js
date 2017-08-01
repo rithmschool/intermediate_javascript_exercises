b4d531075af132b543ecc472778794c3c0623693
Chitwant <chitwant@gmail.com>
2017-08-01 16:46:16 -0700

    recursion 1+2+3+4 sol

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -29,10 +29,11 @@ function contains(obj, num) {
 	var ret = false;
 	function helper(a) {
 		var kArr = Object.keys(a);
-		for (var i=0; i<kArr.length; i++) {
-			if (kArr.length === 0)
-				return;
 
+		if (kArr.length === 0)
+			return;
+
+		for (var i=0; i<kArr.length; i++) {
 			if (typeof a[kArr[i]] === 'object')
 				helper(a[kArr[i]]);
 			else
@@ -42,4 +43,26 @@ function contains(obj, num) {
 	}
 	helper(obj);
 	return ret;
+}
+
+
+function search(arr, num) {
+	var idx = -1;
+	function helper(a, rId) {
+		if (rId === a.length)
+			return;
+
+		if(a[rId] === num) {
+		    idx = rId;
+		}
+		else {
+		    helper(a, ++rId)
+		}
+	}
+	helper(arr, 0);
+	return idx;
+}
+
+function binarySearch(arr, num) {
+
 }
