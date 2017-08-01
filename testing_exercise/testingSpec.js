e020f7bc72ad3867329d37e32b80e001bf71b5ca
Chitwant <chitwant@gmail.com>
2017-07-31 17:31:42 -0700

    function 1 sol

--- testing_exercise/testingSpec.js
+++ testing_exercise/testingSpec.js
@@ -1,3 +1,40 @@
 var expect = chai.expect;
 
 // WRITE YOUR TESTS HERE!
+describe("TESTS FOR replaceWith", function() {
+	it("happy path, character present twice", function() {
+		expect(replaceWith("awesome", "e", "z")).to.equal("awzsomz");
+	});
+
+	it("case sensitive single character", function() {
+		expect(replaceWith("Foo", "F", "B")).to.equal("Boo");
+	});
+
+	it("case sensitive multiple character", function() {
+		expect(replaceWith("FooF", "F", "B")).to.equal("BooB");
+	});
+
+	it("case sensitive single character with char present in lower and upper case", function() {
+		expect(replaceWith("Foof", "F", "B")).to.equal("Boof");
+	});
+
+	it("character not present", function() {
+		expect(replaceWith("awesome", "p", "z")).to.equal("awesome");
+	});
+
+	it("character not present case sensitive", function() {
+		expect(replaceWith("awesome", "E", "Z")).to.equal("awesome");
+	});
+
+	it("input string empty", function() {
+		expect(replaceWith("", "e", "z")).to.equal("");
+	});
+
+	it("replace with Char empty", function() {
+		expect(replaceWith("awesome", "", "z")).to.equal("awesome");
+	});
+});
+
+describe("TESTS FOR expand", function() {
+	
+});
