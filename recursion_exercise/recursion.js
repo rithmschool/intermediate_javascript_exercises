a253fe50572a27177a1d8fa8429913ad9f831a48
Chitwant <chitwant@gmail.com>
2017-08-01 23:00:03 -0700

    recursion 1+2+3+4 sol simple and helper

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -25,24 +25,109 @@ function collectStrings(obj) {
 	return nArr;
 }
 
+// contains with helper recursion
+// function contains(obj, num) {
+// 	var ret = false;
+// 	function helper(a) {
+// 		var kArr = Object.keys(a);
+
+// 		if (kArr.length === 0)
+// 			return;
+
+// 		for (var i=0; i<kArr.length; i++) {
+// 			if (typeof a[kArr[i]] === 'object')
+// 				helper(a[kArr[i]]);
+// 			else
+// 				if (a[kArr[i]] === num)
+// 					ret = true;
+// 		}
+// 	}
+// 	helper(obj);
+// 	return ret;
+// }
+
+// contains with simple recursion
 function contains(obj, num) {
-	var ret = false;
+	var kArr = Object.keys(obj);
+
+	for (var i=0; i<kArr.length; i++) {
+	    if (typeof obj[kArr[i]] === 'object') {
+	        if (contains(obj[kArr[i]], num) === true)
+	            return true;
+	    }
+	    else
+	        if (obj[kArr[i]] === num)
+	            return true;
+	}
+	return false;
+}
+
+// code war problems:
+// 1) real size
+function realSize(arrays) {
+	var nArr = [];
 	function helper(a) {
-		var kArr = Object.keys(a);
-
-		if (kArr.length === 0)
-			return;
-
-		for (var i=0; i<kArr.length; i++) {
-			if (typeof a[kArr[i]] === 'object')
-				helper(a[kArr[i]]);
+		for(var i=0; i < a.length; i++) {
+			if(a[i] instanceof Array)
+				helper(a[i]);
 			else
-				if (a[kArr[i]] === num)
-					ret = true;
+				nArr.push(a[i]);
 		}
 	}
-	helper(obj);
-	return ret;
+	helper(arrays);
+    return nArr.length;
+}
+
+// 2) with helper recursion
+function SumSquares(l){
+    var total = 0;
+    function helper(a) {
+        for(var i=0; i < a.length; i++) {
+            if (a[i] instanceof Array)
+                helper(a[i]);
+            else
+                total += a[i] * a[i];
+        }
+
+    }
+    helper(l);
+    return total;
+}
+
+// 2) with simple recursion 
+function SumSquares(l){
+    var total = 0;
+    for(var i=0; i < l.length; i++) {
+    	if (l[i] instanceof Array)
+    		total += SumSquares(l[i]);
+    	else
+    		total += l[i] * l[i];
+    }
+    return total;
+}
+
+// 3) replicate with helper method recursion
+function replicate(times, number) {
+    var nArr = [];
+    function helper(times, number) {
+        if(times <= 0)
+            return;
+        
+        nArr.push(number);
+        helper(--times, number);
+
+    }
+    helper(times, number)
+    return nArr;
+}
+
+// 3) replicate with simple recursion
+function replicate(times, number) {
+	var arr = [];
+	if (times <= 0) return arr;
+	arr = replicate(times - 1, number);
+    arr.push(number);
+    return arr;
 }
 
 
@@ -52,16 +137,17 @@ function search(arr, num) {
 		if (rId === a.length)
 			return;
 
-		if(a[rId] === num) {
+		if(a[rId] === num)
 		    idx = rId;
-		}
-		else {
-		    helper(a, ++rId)
-		}
+		else
+		    helper(a, ++rId);
 	}
 	helper(arr, 0);
 	return idx;
 }
+
+// search using simple recursion
+
 
 function binarySearch(arr, num) {
 
--- testing_exercise/testing.js
+++ testing_exercise/testing.js
@@ -1,10 +1,8 @@
 function replaceWith(entry, replaceChar, withChar) {
-	if (entry.indexOf(replaceChar) !== -1) {
-		for (var i=0; i < entry.length; i++)
-			if (entry[i] === replaceChar)
-				 entry = entry.substr(0, i) + withChar + entry.substr(i + 1, entry.length - 1);
-	}
-	
+	for (var i=0; i < entry.length; i++)
+		if (entry[i] === replaceChar)
+			entry = entry.substr(0, i) + withChar + entry.substr(i + 1, entry.length - 1);
+
 	return entry;
 }
 
