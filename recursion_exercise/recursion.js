0c597ef098695304f11822b4c0997ae58be8e429
Geoff Gladden <ggladden@gmail.com>
2017-08-01 22:25:13 -0700

    Here's the non-bonus exercises. The nose bleeds prevent any further exercises.

---
+++ recursion_exercise/recursion.js
@@ -0,0 +1,44 @@
+function productOfArray(arr) {
+  if (arr.length == 1) return arr[0];
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+function collectStrings(obj) {
+  let strings = [];
+  
+  function helper(o) {
+    for(let prop in o) {
+      if (typeof o[prop] === 'string') {
+        strings.push(o[prop]);
+      } else {
+        helper(o[prop]);
+      }
+    }  
+  }
+  helper(obj);
+  return strings;
+}
+
+function contains(obj, val) {
+  return true;
+}
+
+function contains(obj, val) {
+  for(let prop in obj){
+    if(typeof obj[prop] === 'object'){
+      if (contains(obj[prop], val)) {
+        return true;
+      }
+    }
+    else if(obj[prop] === val) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Bonus! Er, perhaps when I understand this better.
+//
+function search(arr, val){
+
+}
