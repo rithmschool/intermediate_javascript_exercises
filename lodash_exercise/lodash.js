778753e9c8ab5f46cfe406943fb97c67674d08f2
Matt Barraza-Carl <mccarl@asu.edu>
2017-08-03 10:59:48 -0700

    complete lodash exercises & canvas dir

--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -69,30 +69,46 @@ function includes(collection, target, startingIndex = 0) {
 function sample(array) {
   return Math.round(Math.random(...array) + array[0]);
 }
-//[{ a: 1 }, { b: 2 }]
-function cloneDeep(array) {}
 
+function cloneDeep(collection) {
+  if (Array.isArray(collection)) {
+    return Object.assign([], ...collection);
+  }
+  if (typeof collection === 'object') {
+    return Object.assign({}, collection);
+  }
+}
+//**************************************************//
 function sumBy(array, iteratee) {
   return array && array.length ? iteratee(identity) : 0;
 }
-
+//**************************************************//
 function inRange(num, start, end) {
-  num = Math.floor(num);
-  if (end == undefined) {
-    start = 0;
-  }
   if (num < 0) {
-    return num <= start && num >= end ? true : false;
+    num *= -1;
+    start *= -1;
+    end *= -1;
+  }
+
+  if (aguments.length > 2) {
+    return start < num && num < end;
   } else {
-    return num >= start && num <= end ? true : false;
+    return 0 < num && num < start;
   }
 }
-
+//**************************************************//
 function has(object, other) {
   return object.hasOwnProperty(...other) ? true : false;
 }
 
-function omit(obj, arr) {}
+function omit(obj, arr) {
+  return Object.keys(obj).reduce(function(prev, curr, idx) {
+    if (!arr.includes(curr)) {
+      prev[curr] = obj[curr];
+    }
+    return prev;
+  }, {});
+}
 
 function pick(obj, arr) {
   return arr.reduce(function(newObj, key) {
@@ -102,11 +118,27 @@ function pick(obj, arr) {
     return newObj;
   }, {});
 }
-
-function pickBy(obj, cb) {}
-
-function omitBy() {}
-
+//**************************************************//
+function pickBy(obj, cb) {
+  var o = {};
+  forEach(obj, function(value, key, collection) {
+    if (cb(value)) {
+      o[key] = collection[key];
+    }
+  });
+  return o;
+}
+//**************************************************//
+function omitBy(obj, cb) {
+  var o = {};
+  forEach(obj, function(value, key, collection) {
+    if (!cb(value)) {
+      o[key] = collection[key];
+    }
+  });
+  return o;
+}
+//**************************************************//
 function padEnd(str, target, char) {
   return str.padEnd(target, char);
 }
@@ -120,22 +152,40 @@ function upperFirst(str) {
 }
 
 function flatten(array) {
-  var flat = [];
-  for (var i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) {
-      flat = flat.concat(array[i]);
-    } else {
-      flat.push(array[i]);
-    }
-  }
-  return flat;
+  return array.reduce(function(prev, curr) {
+    return prev.concat(curr);
+  }, []);
+  // var flat = [];
+  // for (var i = 0; i < array.length; i++) {
+  //   if (Array.isArray(array[i])) {
+  //     flat = flat.concat(array[i]);
+  //   } else {
+  //     flat.push(array[i]);
+  //   }
+  // }
+  // return flat;
 }
 
-function zip() {}
+function zip() {
+  return [...arguments].reduce(
+    (prev, curr, idx) => {
+      if (idx === 0) {
+        prev[0].push(curr);
+      } else {
+        prev[1].push(curr);
+      }
+      return prev;
+    },
+    [[], []]
+  );
+}
 
 function unzip() {}
 
-function flip() {
+function flip(flipped) {
+  return flipped();
+}
+function flipped() {
   return [...arguments].reduceRight(function(prev, curr) {
     return prev - curr;
   });
