0ae4f4bacf367bced690f094b1d74a755ce5a65c
Jonathan Yuen <jonwyuen@gmail.com>
2017-08-03 12:01:15 -0700

    updated indentation

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -34,19 +34,18 @@ function realSize(arrays) {
 		for(var i = 0; i < arr.length; i++){
 			if(typeof arr[i] === "number") count++;
   			else if(Array.isArray(arr[i])) helper(arr[i]);
-  		}
   	}
-  	helper(arrays);
-  	return count;
+  }
+  helper(arrays);
+  return count;
 }
-
 
 function SumSquares(l){
 	var total = 0;
-  	function helper(a){
+  function helper(a){
 		for(var i = 0; i < a.length; i++){
 			if(typeof a[i] === "number") total += Math.pow(a[i], 2);
-		  	else if(Array.isArray(a[i])) helper(a[i]);
+		  else if(Array.isArray(a[i])) helper(a[i]);
 		}
 	}
 	helper(l);
@@ -56,7 +55,7 @@ function SumSquares(l){
 function replicate(times, number) {
 	var newArr = [];
  	if(times <= 0) return newArr;
-  	else return newArr.concat(number,replicate(times-1, number));
+  else return newArr.concat(number,replicate(times-1, number));
 }
 
 
@@ -80,29 +79,29 @@ function binarySearch(arr, val, start, end){
 	if(end < start || val < arr[start] || val > arr[end]) return -1;
 	if(val === arr[middle]) return middle;
 	if(val < arr[middle]) return binarySearch(arr, val, start, middle-1);
-  	else return binarySearch(arr, val, middle+1, end);
+  else return binarySearch(arr, val, middle+1, end);
 }
 
 function stringifyNumbers(obj) {
-    var newObj = {};
-    for(var key in obj) {
-        if(typeof obj[key] === "object" && !Array.isArray(obj[key])){
-            newObj[key] = stringifyNumbers(obj[key]);
-        } else if(typeof obj[key] === "number"){
-            newObj[key] = (obj[key]).toString();
-        } else{
-            newObj[key] = obj[key];
-        }
+  var newObj = {};
+  for(var key in obj) {
+    if(typeof obj[key] === "object" && !Array.isArray(obj[key])){
+     	newObj[key] = stringifyNumbers(obj[key]);
+    } else if(typeof obj[key] === "number"){
+    	newObj[key] = (obj[key]).toString();
+    } else{
+     	newObj[key] = obj[key];
     }
-    return newObj;
+  }
+  return newObj;
 }
 
 function F(n) {
-    if (n === 0) return 1;
-    return n - M(F(n - 1));
+  if (n === 0) return 1;
+  return n - M(F(n - 1));
 }
 
 function M(n) {
-    if (n === 0) return 0;
-    return n - F(M(n - 1));
+  if (n === 0) return 0;
+  return n - F(M(n - 1));
 }
