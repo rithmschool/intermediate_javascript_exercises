31b4be41373439202855f7a1c689fbd4f15ebde5
toChaim <toChaim@gmail.com>
2017-08-02 18:07:39 -0700

    fix some tests and my progress so far.

--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -1,41 +1,109 @@
-function drop(){
-
+function drop(arr, skip = 1){
+	return arr.slice(skip);
 }
 
-function fromPairs(){
+function fromPairs(arr){
+	var obj = {};
 
+		for(let p = 0; p < arr.length; p++){
+			obj[arr[p][0]] = arr[p][1];
+		}
+
+	return obj;
 }
 
-function head(){
-
+function head(arr){
+	return arr[0];
 }
 
-function take(){
-
+function take(arr, n = 1){
+	return arr.slice(0, n);
 }
 
-function takeRight(){
-
+function takeRight(arr, n = 1){
+	return arr.reduce(function(t,c,i,a){
+		if(i > arr.length-1-n){
+			t.push(c);
+			return t;
+		}else return t;
+	},[]);
 }
 
 function union(){
+	var resObj = {};
+	  var resArr = [];
+	  
+	  for(let i = 0; i < arguments.length; i++){
+	    let arr = arguments[i];
+	    
+	    for(let v of arr){
+	      var newKey = !resObj.hasOwnProperty(JSON.stringify(v));
+	      if(newKey){
+	        resArr.push(v);
+	        resObj[JSON.stringify(v)] = 'Now I have it.';
+	       }
+	    }
+	    
+	  }
+	  
+	  return resArr;
+}
+
+function zipObject(keys, vals){
+	var obj = {};
+
+	for(let i = 0; i < keys.length; i++){
+		obj[keys[i]] = vals[i];
+	}
+
+	return obj;
+}
+
+function includes(input, val, fromIndex = 0){
+	if(Array.isArray(input)){
+		for(let i = fromIndex; i < input.length; i++) 
+			{
+				if(input[i] === val) 
+					return true;
+			}
+	}else if(typeof input === 'object'){
+		for(let k in input){
+				if(input[k] === val){
+					return true;
+				}
+		}
+	}else if(typeof input === 'string'){
+		for(let i = fromIndex; i < input.length; i++){
+			var temp = input.slice(i, i+ val.length); 
+			if(temp === val) return true;
+		}
+	}
+	
+	return false;
+}
+
+function sample(obj){
+	if(Array.isArray(obj)){
+		return obj[Math.floor(Math.random() * obj.length)];
+	}
+	else{
+		let keys = Object.keys(obj);
+		let k = Math.floor(Math.random() * keys.length);
+		return(obj[keks[k]]);
+	}
 
 }
 
-function zipObject(){
+function cloneDeep(obj){
+	var newObj = {}
+	if(Array.isArray(obj)) newObj = [];
 
-}
+	for(let k in obj){
+		if(typeof obj[k] === 'object'){ newObj[k] = cloneDeep(obj[k]);}
+		else newObj[k] = obj[k];
+	}
 
-function includes(){
-
-}
-
-function sample(){
-
-}
-
-function cloneDeep(){
-
+	return newObj;
 }
 
 function sumBy(){
