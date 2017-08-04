fb02ddd8ec5beafd79cd8b61fd1730271f112685
Jonathan Yuen <jonwyuen@gmail.com>
2017-08-04 01:27:52 -0700

    added lodash and canvas exercises

--- canvas_exercise/shapes_game/index.js
+++ canvas_exercise/shapes_game/index.js
@@ -1,15 +1,56 @@
 window.addEventListener("load", function() {
 
-  function clear(ctx, width, heigt) {
+  function clear(ctx, width, height) {
+    ctx.clearRect(0, 0, width, height);
   }
 
   function drawRandomShape(ctx, width, height) {
+    clear(ctx, canvas.width, canvas.height);
+    var shapes = ["wTri", "wSq", "rTri", "rSq"];
+    var randomShape = shapes[Math.floor(Math.random()*shapes.length)];
+
+    var randomX = Math.floor(Math.random()*(canvas.width - 2*width) + width);
+    var randomY = Math.floor(Math.random()*(canvas.height - 2*height) + height);
+
+
+    if(randomShape === "wSq" || randomShape === "wTri") {
+      ctx.fillStyle = "white";
+    } else {
+      ctx.fillStyle = "red";
+    }
+
+    if(randomShape === "wSq" || randomShape === "rSq") {
+      ctx.fillRect(randomX, randomY, width, height);
+    } else {
+      ctx.beginPath();
+      ctx.moveTo(randomX, randomY);
+      ctx.lineTo(randomX + width, randomY + height);
+      ctx.lineTo(randomX, randomY + height);
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    return randomShape;
   }
 
   function drawGameStartText(ctx, width, height, score) {
+    ctx.fillStyle = "white";
+    ctx.font = '30px arial';
+    ctx.textAlign = "center";
+    ctx.fillText('Press the space bar to start a new game', width/2, height/2);
   }
 
+
   function restartGame(ctx, width, height) {
+    clear(ctx, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.font = '30px arial';
+    ctx.textAlign = "center";
+    ctx.fillText('Press the space bar to play again', width/2, height/2);
+    ctx.fillText("You scored " + score + " points", width/2, height/2 + 50);
+    gameOn = false;
+    score = 0;
+    seconds = 30;
   }
 
   var canvas = document.getElementById("shapes-game"),
@@ -20,17 +61,47 @@ window.addEventListener("load", function() {
       ctx = canvas.getContext('2d'),
       // white triangle = up, red square = down,
       // red triangle = left, white square = right
-      expectedKeysMap = {white0: 38, red1: 40, red0: 37, white1: 39},
+      expectedKeysMap = {wTri: 38, rSq: 40, rTri: 37, wSq: 39},
       timerSpan = document.getElementById("time-remaining"),
       scoreSpan = document.getElementById("score-val"),
-      seconds = 3,
+      seconds = 30,
+      score = 0,
       intervalId;
 
   canvas.width = width;
   canvas.height = height;
 
-  document.addEventListener("keyup", function() {
- 
+  document.addEventListener("keyup", function(e) {
+    e.preventDefault(); //doesn't work
+    if(e.keyCode === 32 && !gameOn){
+      gameOn = true;
+      scoreSpan.innerText = score;
+      timerSpan.innerText = seconds;
+      intervalId = setInterval(function(){
+        seconds--;
+        timerSpan.innerText = seconds;
+      },1000)
+
+      setTimeout(function(){
+        clearInterval(intervalId);
+        clear(ctx, canvas.width, canvas.height);
+        restartGame(ctx, canvas.width, canvas.height);
+      },seconds * 1000);
+
+      expectedKey = drawRandomShape(ctx, 50, 50)
+    } else if(e.keyCode === expectedKeysMap[expectedKey] && gameOn){
+        score++;
+        scoreSpan.innerText = score;
+        expectedKey = drawRandomShape(ctx, 50, 50);
+    } else if(gameOn){
+        score--;
+        scoreSpan.innerText = score;
+        expectedKey = drawRandomShape(ctx, 50, 50);
+    }
   });
+
+  drawGameStartText(ctx, canvas.width, canvas.height)
+
 });
+
 
--- lodash_exercise/lodash.js
+++ lodash_exercise/lodash.js
@@ -1,99 +1,248 @@
-function drop(){
-
+function drop(arr, n=1){
+  return arr.slice(n)
 }
 
-function fromPairs(){
-
+function fromPairs(arr){
+  var obj = {}
+  arr.forEach(function(val){
+    obj[val[0]] = val[1]
+  })
+  return obj;
 }
 
-function head(){
-
+function head(arr){
+  return arr[0];
 }
 
-function take(){
-
+function take(arr, n=1){
+  return arr.slice(0,n);
 }
 
-function takeRight(){
-
+function takeRight(arr, n=1){
+  if(n >= arr.length) n = arr.length;
+  return arr.slice(arr.length-n)
 }
 
-function union(){
-
+function union(...arrays){
+  var newArr = [];
+  arrays.forEach(function(val){
+    val.forEach(function(e){
+      if(newArr.indexOf(e) === -1){
+        newArr.push(e);
+      }
+    })
+  })
+  return newArr;
 }
 
-function zipObject(){
+function zipObject(arr1,arr2){
+  return arr1.reduce(function(acc,next,idx){
+    acc[next] = arr2[idx];
+    return acc;
+  },{})
+} 
 
+function includes(col, val, idx=0){
+  if(typeof col === "object" && !Array.isArray(col)){
+    for(var key in col){
+      if(col[key] === val){
+        return true; 
+      }
+    }
+    return false;
+  }
+  if(Array.isArray(col)){
+    if(idx>0){
+      col = col.slice(idx)
+    }
+    return col.indexOf(val) >=0;
+  }
+  if(typeof col === "string"){
+    return col.indexOf(val) >=0;
+  }
 }
 
-function includes(){
-
+function sample(col){
+  var objArr = [];
+  if(typeof col === "object" && !Array.isArray(col)){
+    for(var key in col){
+      objArr.push(col[key]);
+    }
+    return objArr[Math.floor(Math.random()*objArr.length)];
+  }
+  if(Array.isArray(col)){
+    return col[Math.floor(Math.random()*col.length)];
+  }
 }
 
-function sample(){
-
+function cloneDeep(val){
+  var obj = {};
+  var arr = [];
+  if(typeof val === "object"){
+    for(var i = 0; i < val.length; i++){
+      for(var key in val[i]){
+        if(typeof val === "object"){
+          obj[key] = cloneDeep(val[i][key])
+        } else{
+          obj[key] = val[i][key];
+        }
+      }
+      arr.push(obj)
+    }
+  return arr; 
+  } 
 }
 
-function cloneDeep(){
-
+function sumBy(arr,itr){
+  if(typeof itr === "function"){
+    return arr.reduce(function(acc,next){
+      acc += itr(next);
+      return acc;
+    },0);
+  } else if(typeof itr === "string"){
+    return arr.reduce(function(acc,next){
+      return acc + next[itr];
+    },0);
+  }
 }
 
-function sumBy(){
 
+function inRange(num, start=0, end){
+  if(arguments.length === 2){
+    end = start;
+    start = 0;
+  }
+  return num>=Math.min(start,end) && num<Math.max(start,end);
 }
 
-function inRange(){
 
+function has(obj,path){
+  if(typeof path === "string"){
+    for(var key in obj){
+      return key === path;
+    }
+  }
+  if(Array.isArray(path)){
+    for(var key in obj){
+      return path.includes(key);
+    }
+  }
 }
 
-function has(){
-
+function omit(obj, paths){
+  var newObj = {};
+  for(var key in obj){
+      if(paths.indexOf(key) === -1){
+          newObj[key] = obj[key]
+      }
+  }
+  return newObj;
 }
 
-function omit(){
-
+function pick(obj, paths){
+  var newObj = {};
+  for(var key in obj){
+      if(paths.indexOf(key) >= 0){
+          newObj[key] = obj[key]
+      }
+  }
+  return newObj;
 }
 
-function pick(){
-
+function pickBy(obj, fn){
+  var newObj = {};
+  for(var key in obj){
+    if(fn(obj[key])){
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
 }
 
-function pickBy(){
-
+function omitBy(obj, fn){
+  var newObj = {};
+  for(var key in obj){
+    if(!fn(obj[key])){
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
 }
 
-function omitBy(){
-
+function padEnd(str,num,padding){
+  if(arguments.length === 2) padding = " ";
+  if(str.length + padding.length === num){
+    return str.concat(padding);
+  }
+  if(str.length + padding.length < num){
+    str = str.concat(padding);
+    return padEnd(str,num,padding);
+  }
+  else if(str.length + padding.length > num){
+    return str.concat(padding).slice(0,num);
+  } 
 }
 
-function padEnd(){
 
-}
-
-function repeat(){
-
+function repeat(str,num){
+  var newStr = "";
+  for(var i = 0; i < num; i++){
+    newStr = newStr.concat(str);
+  }
+  return newStr;
 }
 
 function upperFirst(str){
-
+  return str[0].toUpperCase().concat(str.slice(1));
 }
 
-function flatten(){
-
+function flatten(arr){
+  return arr.reduce(function(acc,next){
+    return acc.concat(next);
+  },[]);
 }
 
-function zip(){
 
+function zip(...arr){
+  var resultArr = [];
+  arr[0].forEach(function(val,idx){
+    var holderArr = [];
+    arr.forEach(function(v,i){
+      holderArr.push(arr[i][idx]);
+    })
+  resultArr.push(holderArr);
+  })
+  return resultArr;
 }
 
-function unzip(){
-
+function unzip(...arr){
+  var resultArr = [];
+  arr[0][0].forEach(function(val,idx){
+    var holderArr = [];
+    arr[0].forEach(function(v,i){
+      holderArr.push(arr[0][i][idx]);
+    })
+  resultArr.push(holderArr);
+  })
+  return resultArr;
 }
 
-function flip(){
 
+function flip(fn){
+  return function(...args){
+    return fn(...args.reverse());
+  }
 }
 
-function flattenDeep(){
-
+function flattenDeep(arr){
+  var newArr = [];
+  function helper(a){
+    for(var i = 0; i < a.length; i++){
+      if(Array.isArray(a[i])) helper(a[i]);
+      else newArr.push(a[i]);
+    }
+  }
+  helper(arr);
+  return newArr;
 }
+
