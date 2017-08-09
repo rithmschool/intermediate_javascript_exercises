fe30029872a311a73298b624f91b057c213281f5
toChaim <toChaim@gmail.com>
2017-08-09 08:48:25 -0700

    got get of favoriets array working. improve login click.

--- ajax_with_jquery_exercise/index.js
+++ ajax_with_jquery_exercise/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 	var $links = $('#links');
+	var $login_link = $('#login-link');
 	var $login_form = $('#login-form');
 	var $inp_email = $('#inp-email');
 	var $inp_password = $('#inp-password');
 	var authorization = '';
 
-	$('#login-link').click(() => { $login_form .toggleClass("hidden") });
+	$('#login-link').click(() => { $login_form .toggleClass("hidden"); });
 	$('#login-btn').click(function(e){
 		e.preventDefault();
 		//var email = "lane.matthew@gmail.com";
@@ -29,22 +30,32 @@ $(document).ready(function(){
 			authorization = data.auth_token;
 			console.log(data.auth_token);
 			$('.hideNoLogin').toggleClass('hideNoLogin showOnLogin');
-			//getFavorites(data.auth_token);
+			$login_link.text('Log Out');
+			$inp_email.val('');
+			$inp_password.val('');
+			$login_form .toggleClass("hidden");
+			return $.ajax(
+			{
+		    	method: "GET",
+		    	headers: { "Authorization": authorization },
+		      	'url': url,
+			});
 		})
+		.then()
 		.fail(err => console.warn(err));
 	});
 
 	getStories();
 
-	function getStories(login = '', favorites = false){
+	function getStories(login = '', favorites = []){
 		var url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
-		console.log('login is ' + login);
 
 		$.get(url)
 		.then(function(res){
 			for(let i = 0; i < 20; i++){
 				var newUrl = `https://hacker-news.firebaseio.com/v0/item/${res[i]}.json?print=pretty`;
 				 $.get(newUrl).then(function(res){
+				 	if(favorites)
 					displayLink(res.title, res.url)
 				});	
 			}
@@ -56,18 +67,16 @@ $(document).ready(function(){
 	}
 
 	function getFavorites(login) {
-		var url = 'https://hacker-news.firebaseio.com/favorites.herokuapp.com/stories.json';
+		var url = 'https://hn-favorites.herokuapp.com/stories.json';
 		
 		$.ajax({
-		    method: "POST",
+		    method: "GET",
 		    headers: {
-		        "Content-Type": "application/json",
 		        "Authorization": authorization,
 		      },
 		    'url': url,
 		})
 		.then(function(data) { 
-			console.log(data);
 		})
 		.fail(err => console.warn(err));
 
@@ -78,7 +87,7 @@ $(document).ready(function(){
 		let shortLink = '';
 		if(link)  { shortLink = link.split('/').filter(v => v.includes('.')); }
 		if(favor) html =  '<a class="glyphicon glyphicon-star star hideNoLogin" href="#"></a>';
-			{ html += `<a href="${link}">${title}<span>(${shortLink[0]})</span></a>`; }
+			{ html += `<a href="${link}">${title}</a> <small>( ${shortLink[0]} )</small>`; }
 		let $lin = $('<li>');
 		$lin.html(html);
 		if(favor) $lin.addClass('favor');
