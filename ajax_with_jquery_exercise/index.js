d7a7d5236bfde5cf5ef24a17f8d584fbf499c784
Brian Harris <brianharrisdesign@gmail.com>
2017-08-09 22:28:57 -0700

    Broken! POST list.on returns Bad

--- ajax_with_jquery_exercise/index.js
+++ ajax_with_jquery_exercise/index.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
 	console.log("Logged In: " + loggedIn);
 
 	var $list = $("#list_links");
-	var $favs = $("#list_favs");
-	var $lnkFavorites = $("#link_fav");
 	var $lnkLogin = $("#link_login");
 	var $lnkSignup = $("#link_signup");
 	var $loginDiv = $("#login_div");
 	var $signupDiv = $("#signup_div");
 	var $btnLogin = $("#login_form");
 	var $btnSignup = $("#signup_form");
+
+	// FAVORITES
+	var $favs = $("#list_favs");
+	var $lnkFavorites = $("#link_fav");
+	var favContainer = {};
 
 	/* DEV SETTINGS */
 	//localStorage.removeItem("hos_top10Cached");
@@ -79,7 +82,7 @@ $(document).ready(function() {
 				alert("Log In: Please enter a valid email address and password!");
 			});
 		}
-	})
+	});
 
 	$btnSignup.on('click', function(event) {
 		var username = $("#input_text3").val();
@@ -121,7 +124,44 @@ $(document).ready(function() {
 	$list.on('click', function(event){
 		if ($(event.target).attr('type') === 'checkbox') {
 			if ($(event.target).is(':checked')) {
-				// REQUEST IT BE A FAVORITE
+				
+				var $siblings = $(event.target).siblings(); // Get Parent
+				var $key = $(event.target).attr('id');
+				// Add this checkbox to the local container of favorites.
+				favContainer[$key] = {
+					$url: $($siblings[1]).text().replace("(", "").replace(")", ""),
+					$by: $($siblings[2]).text().slice(3),
+					$title: $($siblings[0]).text()
+				}
+
+				// FAIL
+				$.ajax({
+			    method: "POST",
+			    headers: {
+			        "Content-Type": "application/json",
+			        "Authorization": localStorage.getItem("hos_autoToken")
+			      },
+			    url: "https://hn-favorites.herokuapp.com/stories.json",
+			    data: {
+		        hacker_news_story: {						///// THIS IS FAILING. JSON Turns this into a bad string
+		        	by: favContainer.$by,
+		        	story_id: favContainer.$key,
+		        	title: favContainer.$title,
+		        	url: favContainer.$url
+		        }
+		    	}
+				})
+				.then(function(data) { 
+					console.log("Favorite Data Returned: " + data);
+				})
+				.fail(function(err) {
+					var loc = localStorage.getItem("hos_autoToken")
+					console.warn("Add Favorite Error: " + err);
+					//alert("Log In: Please enter a valid email address and password!");
+				});
+
+				
+
 			} else {
 				// REQUEST IT BE REMOVED FROM FAVORITES
 			}
@@ -140,98 +180,14 @@ $(document).ready(function() {
 			console.log(response);
 
 			// HERE IS WHERE WE WILL FILL THE FAVORITES LIST
+			// Also hide the regular list and show the favorites only.
+		})
+		.fail(function(response) {
+			// What happens when we fail?
 		});
 	});
 
-	/*$btnSubmit.on('click', function() {
 
-	});*/
-
-	//$btnSubmit.on('click');
-	/*var $linkSubmit = $("#link_submit");
-	var $formSubmit = $("#submission");
-	
-	
-	var $btnFavorites = $("#link_fav");
-	var favoritesOnly = false;
-	var urlFilterOn = false;*/
-
-	/*$linkSubmit.on("click", function() {
-		$formSubmit.eq(0).slideToggle(250);
-	});
-
-	$btnSubmit.on("submit", function (e) {
-		e.preventDefault();
-
-		// GRAB THE TEXT BOXES HERE
-		var $title = $('#input_text1').val();
-		var $url = ($('#input_text2').val()).replace("http://", "");
-
-		var $listItem = $("<li>", {
-		});
-
-		$listItem.html(`<input type="checkbox">
-			<span>${$title}</span>
-			<span class="tiny_text">(<a href="#" class="link_filter">${$url}</a>)</span>
-		`);
-
-		$list.append($listItem);
-
-		$formSubmit.eq(0).slideToggle(250);
-	});
-
-	$btnFavorites.on("click", function() {
-		favoritesOnly = !favoritesOnly;
-
-		$("#list_links li").each(function() {
-			if (urlFilterOn === false) {
-				var $checkbox = $(this).children().eq(0);
-
-				if ($checkbox.prop("checked") === false) {
-					$(this).toggleClass("visible invisible");
-				} 
-			} else {
-				$(this).addClass("visible");
-				$(this).removeClass("invisible");
-			}
-		});
-
-		if (urlFilterOn) {
-			urlFilterOn = false;
-			favoritesOnly = false;
-			$btnFavorites.text("favorites");
-		}
-
-		if (favoritesOnly === true) {
-			$list.css("list-style-type", "none");
-		} else {
-			$list.css("list-style-type", "decimal");
-		}
-
-		// Just in case this was off!
-		$list.on('click', ".link_filter", listCallback);
-	});
-
-	$list.on('click', ".link_filter", listCallback);
-
-	function listCallback(e) {
-		$btnFavorites.text("all");
-		urlFilterOn = true;
-		favoritesOnly = false;
-		var $urlLink = $(e.target).text();
-
-
-		$("#list_links li").each(function() {
-			var $linkText = $(this).children().eq(2).children().eq(0).text();
-			if ($linkText !== $urlLink) {
-				$(this).toggleClass("visible invisible");
-			};
-		});
-
-		$list.css("list-style-type", "none");
-		$list.off('click');
-
-	};*/
 });
 
 function createTopStoryList(n) {
@@ -258,14 +214,13 @@ function buildList(responseList, loggedIn) {
 	responseList.forEach(function(val) {
 		$listItem = $("<li>", {
 		});
-		
-		//url = val.url;
+
 		index = val.url.indexOf("://");
 		url = val.url.slice(index+3);
 		index = url.indexOf("/");
 		url = url.slice(0, index);
 
-		$listItem.html(`<input type="checkbox" class="${visible}">
+		$listItem.html(`<input id=${val.id} type="checkbox" class="${visible}">
 			<span>${val.title}</span>
 			<span class="tiny_text">(<a href="#" class="link_filter">${url}</a>)</span>
 			<div class="text_by">by ${val.by}</div>
