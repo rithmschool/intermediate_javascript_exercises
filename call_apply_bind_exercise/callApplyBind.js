2a5ed0ec5d2ad9259d2e8b9b73ccc3c9fb1b18ce
Jonathan Yuen <jonwyuen@gmail.com>
2017-08-09 17:10:56 -0700

    updated call apply bind exercises

--- call_apply_bind_exercise/callApplyBind.js
+++ call_apply_bind_exercise/callApplyBind.js
@@ -3,19 +3,19 @@
 var obj = {
   fullName: "Harry Potter",
   person: {
-  	sayHi: function(){
-    	return "This person's name is " + this.fullName
-  	}
-	}
+    sayHi: function(){
+      return "This person's name is " + this.fullName
+    }
+  }
 }
 
 //Fix
 
 var obj = {
- 	fullName: "Harry Potter",
- 	sayHi: function(){
+  fullName: "Harry Potter",
+  sayHi: function(){
     return "This person's name is " + this.fullName
- 	}
+  }
 }
 
 obj.sayHi();
@@ -27,12 +27,12 @@ obj.sayHi();
 //3. Write a function called sumEvenArguments which takes all of the arguments passed to a function and returns the sum of the even ones.
 
 function sumEvenArguments(){
-	return [].slice.call(arguments).reduce(function(acc,next){
-		if(next % 2 === 0){
-			acc += next;
-		}
-		return acc;	
-	},0)
+  return [].slice.call(arguments).reduce(function(acc,next){
+    if(next % 2 === 0){
+      acc += next;
+    }
+    return acc; 
+  },0)
 }
 
 /*sumEvenArguments(1,2,3,4) // 6
@@ -51,10 +51,10 @@ sumEvenArguments(1,2) // 2*/
 // }
 
 function arrayFrom(){
-	return [].slice.call(arguments);
+  return [].slice.call(arguments);
 }
 
-//5. Write a function called invokeMax which accepts a function and a maximum amount. invokeMax should return a function that when called increments a counter. If the counter is greater than the maximum amount, the inner function should return "Maxed Out!"
+//5. Write a function called invokeMax which accepts a function and a maximum amount. invokeMax should return a function that when called increments a counter. If the counter is greater than the maximum amount, the inner function should return "Maxed Out"
 
 /*function add(a,b){
     return a+b
@@ -67,14 +67,14 @@ addOnlyThreeTimes(1,2) // 3
 addOnlyThreeTimes(1,2) // "Maxed Out!"*/
 
 function invokeMax(fn,max){
-	var counter = 0;
-	return function(){
-		counter++;
-		if(counter > max){
-			return "Maxed Out!";
-		}
-		return fn.apply(this,arguments);
-	}
+  var counter = 0;
+  return function(){
+    counter++;
+    if(counter > max){
+      return "Maxed Out!";
+    }
+    return fn.apply(this,arguments);
+  }
 }
 
 //6. Write a function called guessingGame which takes in one parameter amount. The function should return another function that takes in a parameter called guess. In the outer function, you should create a variable called answer which is the result of a random number between 0 and 10 as well as a variable called guesses which should be set to 0.
@@ -98,20 +98,20 @@ game2(1) // "You are all done playing!*/
 
 
 function guessingGame(amount){
-	var answer = Math.floor(Math.random()*11);
-	var guesses = 0;
-	return function(guess){
-		guesses++;
-		if(guesses > amount){
-			return "You are all done playing!";
-		} else{
-			if(guess === answer){
-				return "You got it!";
-			} else if(guess > answer){
-				return "You're too high!";
-			} else{
-				return "You're too low!";
-			}
-		}
-	}
+  var answer = Math.floor(Math.random()*11);
+  var guesses = 0;
+  return function(guess){
+    guesses++;
+    if(guesses > amount){
+      return "You are all done playing!";
+    } else{
+      if(guess === answer){
+        return "You got it!";
+      } else if(guess > answer){
+        return "You're too high!";
+      } else{
+        return "You're too low!";
+      }
+    }
+  }
 }
