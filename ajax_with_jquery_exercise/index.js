ed8a7e3d24f3858bdb97a015477f44629b12adeb
toChaim <toChaim@gmail.com>
2017-08-11 01:16:45 -0700

    tic,tac,toe working

--- ajax_with_jquery_exercise/index.js
+++ ajax_with_jquery_exercise/index.js
@@ -1,103 +1,62 @@
 $(document).ready(function(){
-	var $links = $('#links');
-	var linksList = [];
-	var favList = [];
-	var $login_link = $('#login-link');
-	var $login_form = $('#login-form');
-	var $inp_email = $('#inp-email');
-	var $inp_password = $('#inp-password');
-	var authorization = '';
+	//variables
+	var auth_token;
 
-	$('#login-link').click(() => { $login_form .toggleClass("hidden"); });
-	$('#login-btn').click(function(e){
-		e.preventDefault();
-		//var email = "lane.matthew@gmail.com";
-		var email = $inp_email.val();
-		//var password = "password";
-		var password = $inp_password.val();
+	//imeadiat calls
+	getTop().then(function(data){
+		debugger 
+	})
 
-		$.ajax({
-		    method: "POST",
-		    headers: {
-		        "Content-Type": "application/json"
-		      },
-		    url: "https://hn-favorites.herokuapp.com/login",
-		    data: JSON.stringify({
-		        'email': email, 
-		        'password': password
-		    })
-		})
-		.then(function(data) { 
-			localStorage.setItem('auth_token', data.auth_token);
-			authorization = data.auth_token;
-			console.log(data.auth_token);
-			$login_link.text('Log Out');
-			$inp_email.val('');
-			$inp_password.val('');
-			$login_form .toggleClass("hidden");
-			return $.ajax(
-			{
-		    	method: "GET",
-		    	headers: { "Authorization": authorization },
-		      	'url': 'https://hn-favorites.herokuapp.com/stories.json',
-			});
-		})
-		.then(function(data){
-			getStories(true, data);
-		})
-		.fail(err => console.warn(err));
-	});
+	//click functions
 
-	getStories();
-
-	function getStories(login = false, favorites = []){
-		var url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
-
-		$.get(url)
-		.then(function(res){
-			$links.empty();
-			for(let i = 0; i < 20; i++){
-				var newUrl = `https://hacker-news.firebaseio.com/v0/item/${res[i]}.json?print=pretty`;
-				 $.get(newUrl).then(function(res){
-					displayLink(res.title, res.url, res.id, false, login);
-				});	
-			}
-			
+	//functions
+	function getTop(){
+		return $.get('https://hacker-news.firebaseio.com/v0/'
+			+ 'topstories.json?print=pretty')
+		.then(function(topstories){
+			console.log(topstories);
+			return Promise.all(topstories.slice(0,50).map(function(v,i){
+				return $.get(
+					`https://hacker-news.firebaseio.com/v0/' 
+					+ 'item/${topstories[i]}.json?print=pretty`)
+			}));
 		})
 		.fail(function(err){
-			console.warn(err);
+			console.warn(err); 
+			return err;
 		});
+
 	}
 
-	function getFavorites(login) {
-		var url = 'https://hn-favorites.herokuapp.com/stories.json';
-		
-		$.ajax({
+	function getFav() {
+		return $.ajax({
 		    method: "GET",
 		    headers: {
-		        "Authorization": authorization,
+		        "Authorization": auth_token,
 		      },
-		    'url': url,
+		    'url': 'https://hn-favorites.herokuapp.com/stories.json',
 		})
-		.then(function(data) { 
-		})
-		.fail(err => console.warn(err));
 
 	}
 
-	function displayLink(title, link, storyId=0, favor = false, login = false){
-		var loginClass = 'hideNoLogin';
-		if(login) loginClass = 'showOnLogin';
-		let html = `<a storyId="${storyId}" class="glyphicon glyphicon-star-empty star ${loginClass}" href="#"></a>`;
-		let shortLink = '';
-		if(link)  { shortLink = link.split('/').filter(v => v.includes('.')); }
-		if(favor) html =  `<a storyId="${storyId}" class="glyphicon glyphicon-star star ${loginClass}" href="#"></a>`;
-			{ html += `<a href="${link}">${title}</a> <small>( ${shortLink[0]} )</small>`; }
-		let $lin = $('<li>');
-		$lin.html(html);
-		if(favor) $lin.addClass('favor');
-		$lin.addClass('bg-warning')
-		$links.append($lin);
+	function getLogin(email, password){
+		$.ajax({
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			url: "https://hn-favorites.herokuapp.com/login",
+			data: JSON.stringify({
+				'email': email, 
+				'password': password
+			})
+		}).then(function(data) { 
+			localStorage.setItem('auth_token', data.auth_token);
+			auth_token = data.auth_token;
+			console.log(data.auth_token);
+		})
+
+
 	}
 
 });
--- intermediate_oop/tic_tac_toe/js/main.js
+++ intermediate_oop/tic_tac_toe/js/main.js
@@ -1,2 +1,132 @@
 document.addEventListener("DOMContentLoaded", function() {
+	//Imediate Calls
+	var game = new Game();
+
+	//set up web clicks
+	$('#new-game').click(function(){
+		game.reset();
+	})
+	
+
+	//Functions
+	function Game(){
+		this.player = 'X';
+		this.board = new Board(this);
+		this.gameOn = true;
+	}
+	Game.prototype.reset = function(){
+		this.player = 'X';
+		this.board.clear();
+		this.gameOn = true;
+	}
+	Game.prototype.play = function(row, col){
+		//check for leagal move
+		if(this.gameOn && this.board.isEmpty(row,col)){
+			//mark play
+			this.board.play(row,col,this.player);
+			//check for win or tie
+			if(this.winLooseOrDraw()){
+				this.gameOn = false;
+				alert(this.winLooseOrDraw());
+			}
+			//change players
+			this.player = (this.player === 'X'? 'O':'X');
+		}
+	}
+	Game.prototype.winLooseOrDraw = function(){
+		let ties = 0;
+		for(let row = 0; row < 3; row++){
+			let count = {X:0, O:0, "":0};
+			for(let col = 0; col < 3; col++){
+				count[this.board.get(row, col)]++;
+			}
+			if(count['X']>0 && count['O']>0) ties++;
+			else if(count['X'] === 3) return 'X Wins!';
+			else if(count['O'] === 3) return 'O Wins!';
+			//console.log(count,ties);
+		}
+		for(let col = 0; col < 3; col++){
+			let count = {X:0, O:0, "":0};
+			for(let row = 0; row < 3; row++){
+				count[this.board.get(row, col)]++;
+			}
+			if(count['X']>0 && count['O']>0) ties++;
+			else if(count['X'] === 3) return 'X Wins!';
+			else if(count['O'] === 3) return 'O Wins!';
+			//console.log(count,ties);
+		}
+		let count = {X:0, O:0, "":0};
+		for(let row = 0, col=0; row < 3; row++, col++){
+			count[this.board.get(row, col)]++;
+		}
+		if(count['X']>0 && count['O']>0) ties++;
+		else if(count['X'] === 3) return 'X Wins!';
+		else if(count['O'] === 3) return 'O Wins!';
+		count = {X:0, O:0, "":0};
+		for(let row = 2, col=0; row >= 0; row--, col++){
+			count[this.board.get(row, col)]++;
+		}
+		if(count['X']>0 && count['O']>0) ties++;
+		else if(count['X'] === 3) return 'X Wins!';
+		else if(count['O'] === 3) return 'O Wins!';
+
+		if(ties === 8) return 'Tie Game';
+		else return false;
+	}
+
+	function Board(game){
+		this.game = game;
+		this.$board = $('#board');
+				this.squares = [];
+
+		for(let r = 0; r < 3; r++){
+			let row = [];
+			for(let col = 0; col < 3; col++){
+				row.push(new Squares($(`#square_${r}_${col}`)));
+			}
+			this.squares.push(row);
+		}
+
+		//help binding is not working
+		//var play = game.play.bind(this);
+		//console.log(this.game);
+		this.$board.on('click', '.square', function(e){
+			var [row, col] = e.target.id.split('_').slice(1);
+			game.play(row,col);
+		});
+
+	}
+	Board.prototype.clear = function(){
+		for(let row = 0; row < 3; row++){
+			for(let col = 0; col < 3; col++){
+				this.squares[row][col].clear();
+			}
+		}
+	}
+	Board.prototype.get = function(row, col){
+		return this.squares[row][col].get();
+	}
+	Board.prototype.isEmpty = function(row, col){
+		return this.squares[row][col].isEmpty();
+	}
+	Board.prototype.play = function(row, col, player){
+		this.squares[row][col].set(player);
+	}
+
+	function Squares($jObj){
+		this.$jObj = $jObj;
+	}
+	Squares.prototype.clear = function(){
+		this.$jObj.text('');
+	}
+	Squares.prototype.isEmpty = function(){
+		return this.$jObj.text() === '';
+	}
+	Squares.prototype.set = function(val){
+		this.$jObj.text(val);
+	}
+	Squares.prototype.get = function(){
+		return this.$jObj.text();
+	}
+
 });
