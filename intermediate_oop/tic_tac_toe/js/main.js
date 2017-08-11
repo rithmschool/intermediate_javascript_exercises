b8a49cf3f57c3639e3905c5c2d06a74d098c1f5b
Adele Landers <adelethefish@gmail.com>
2017-08-11 16:30:40 -0700

    Re-do of TicTacToe

--- intermediate_oop/tic_tac_toe/js/main.js
+++ intermediate_oop/tic_tac_toe/js/main.js
@@ -1,105 +1,43 @@
-// $(document).ready(function(){
-// 	$("#board").on("click",".square", function(e){
-// 		console.log($(e.target).html("x")); 
-// 	});
-// });
+//click event
+
+$(document).ready(function(){
+	var gameMaster = new GameMaster();
+});
 
 
-//Click EVENTS
-var clickEvents = {
-	//when new game button is clicked, GameStarter() ?
-    init: function() {
-        	//$("board").empty();
-        	$("#new-game").on("click", function(e){
-        		$(".square").empty();
-		    	console.log($(e.target).val()); 
-		    });
-        //clickEvents.setup();
-    },
- 
- 	//when each square id is clicked, draw X or O depending on what player played
- 	//last, and change state of square
-    writeXorO: function() {
-        $("#board").on("click",".square", function(e){
-			console.log($(e.target).html("x")); 
-			//change status of that squre to filled
-		});
-    },
-};
-
-
-//Board class
-function Board(squares){
-	this.filledSquare = 0;
-	this.squares = squares;
-	this.grid = []
-	console.log(this)
-}
-
-//will build board with jQuery listener on each div
-Board.prototype.buildBoard = function(){
-	$(document).ready(clickEvents.init);
-	for(i = 0; i < 9; i++){
-		this.grid[i] = new Square;
-	}
-	
-}
-//will update board
-Board.prototype.update = function(row,column,status){
-	this.grid[row][column]
-}
-//make marks on board
-
-//display the board at current state
-
-
-//Game class for "rules" or gameplay
-//will decide win, lose, or tie
 function GameMaster(){
-	this.board = new Board();
-	this.board.buildBoard();
-	this.players = []
-	this.players[0] = new Player("X");
-	this.players[1] = new Player("O");
-
+	this.board = new Board(this);
+	this.player = "X"
 }
 
-//GameStarter class to start or clear board
-function GameStarter(){
-	var beginGame = new GameMaster();
-	beginGame;
-}
-//starts game
-GameStarter();
-
-//Player class, makes each player 
-function Player(player){
-	this.player = player;
-}
-
-
-function Square(){
-	//status - can be X, O, or ""
-	this.status = "";
-}
-
-Square.prototype.makeMark = function(player = "X"){
-	//if it's not empty("E") (it's occupied)
-	if(this.status !== "E"){
-		alert("The square is already filled");
+//shared variables
+GameMaster.prototype.play = function(sq){
+	sq.play(this.player);
+	if(this.player === "X"){
+		this.player = "O"
 	} else {
-		$(document).ready(clickEvents.writeXorO);
-		this.status = "X"
+		this.player = "X"
 	}
 }
 
-
-Square.prototype.getMark = function(){
-	return this.mark
+function Board(gameMaster){
+	this.grid = [];
+	this.gameMaster = gameMaster;
+	for(i = 0; i < 9; i++){
+		this.grid[i] = new Square(i,gameMaster);
+	}
 }
 
-//make move class
-function Move(){
-	this.x = x
-	this.o = o
+function Square(x, gameMaster){
+	this.status = "";
+	this.$position = $(`#square_${x}`)
+	this.gameMaster = gameMaster; 
+	this.$position.on("click", function(){
+		this.gameMaster.play(this);
+	}.bind(this));
+}
+
+Square.prototype.play = function(mark){
+	this.status = mark
+	this.$position.text(mark);
 }
