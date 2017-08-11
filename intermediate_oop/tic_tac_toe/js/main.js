ba9384acc4a9981d78e2f55050a269accbf2b394
Geoff Gladden <ggladden@gmail.com>
2017-08-10 23:07:40 -0700

    Tic Tac Toe, OOP style. Or more specifically, OOP JS as written by a crazy person.

--- intermediate_oop/tic_tac_toe/js/main.js
+++ intermediate_oop/tic_tac_toe/js/main.js
@@ -1,2 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var game = new Game();
+  game.newGame();
+  
+  document.getElementById('new-game').addEventListener('click', function(){
+    game.newGame();
+  });
 });
