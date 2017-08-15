22e919a2a9cc25aee025a465985bdec336022805
Matt <mccarl@asu.edu>
2017-08-15 00:33:54 -0700

    Refactor Func.proto.bind & add reduce function
    http://reactivex.io/learnrx => open dev tools => cmd+f 'reduce'  for reduce

--- prototypes_exercise/prototypes.js
+++ prototypes_exercise/prototypes.js
@@ -35,8 +35,44 @@ String.prototype.reverse = function() {
 	return this.split('').reverse().join('')
 }
 
-function bind(func, thisValue) {
-    return function() {
-        return func.apply(thisValue, arguments);
-    }
+// function bind(func, thisValue) {
+//     return function() {
+//         return func.apply(thisValue, arguments);
+//     }
+// }
+Function.prototype.bind = function (fn, thisArg){
+  return function(){
+    return fn.apply(thisArg, arguments);
+  }
 }
+
+Array.prototype.reduce = function(combiner, initialValue) {
+  var counter,
+    accumulatedValue;
+
+  // If the array is empty, do nothing
+  if(this.length === 0) {
+    return this;
+  } else {
+    // If the user didn't pass an initial value, use the first item.
+    if(arguments.length === 1) {
+      counter = 1;
+      accumulatedValue = this[0];
+    } else if(arguments.length >= 2) {
+      counter = 0;
+      accumulatedValue = initialValue;
+    } else {
+      throw "Invalid arguments.";
+    }
+
+    // Loop through the array, feeding the current value and the result of
+    // the previous computation back into the combiner function until
+    // we've exhausted the entire array and are left with only one function.
+    while(counter < this.length) {
+      accumulatedValue = combiner(accumulatedValue, this[counter])
+      counter++;
+    }
+
+    return [accumulatedValue];
+  }
+};
