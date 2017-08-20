41d1ac6b7d31dc1f3d04a1b9b6485c41e21398eb
Renwick Young <young.renwick@gmail.com>
2017-08-20 01:17:35 -0700

    ajax assignment completed, pls review.

--- ajax_with_jquery_exercise/script.js
+++ ajax_with_jquery_exercise/script.js
@@ -5,11 +5,12 @@
   //URLs
    var urls = {
     topStories : 'https://hacker-news.firebaseio.com/v0/topstories.json',
-    newsStories: 'https://hacker-news.firebaseio.com/v0/newstories.json',
+    newStories: 'https://hacker-news.firebaseio.com/v0/newstories.json',
     bestStories: 'https://hacker-news.firebaseio.com/v0/beststories.json',
     itemStories: 'https://hacker-news.firebaseio.com/v0/item/',      //  id + .json  --> get detail
     favoritesStories :  'https://hn-favorites.herokuapp.com/stories.json',
-    favoritesDelete :  'https://hn-favorites.herokuapp.com/stories',  // id + .json  --> delete + ID.json
+    favoritesAdd :  'https://hn-favorites.herokuapp.com/stories.json',
+    favoritesDelete :  'https://hn-favorites.herokuapp.com/stories/',  // id + .json  --> delete + ID.json
     favoritesLogin: 'https://hn-favorites.herokuapp.com/login',
     favoritesSignup: 'https://hn-favorites.herokuapp.com/signup'
   }
@@ -23,14 +24,14 @@
   }
 
 //Content Display Settings
-  var numberGetStories = 5;
+  var numberGetStories = 20;
   var lastStoryDisplayed = 0;
 
 //Buttons
-  var $postUrlForm = $('#newPost');
   var $navLoginBtn = $('#loginSignUpBtn');
   var $mainLoginForm = $('#loginSignUpForm');
   var $dataDisplay = $('ol');
+  var $navBtns = $('ul');
 
   //Login Sign Up Form Buttons
   var $loginSignUpTabs = $('.nav-tabs');
@@ -38,35 +39,35 @@
   var $formSignUp =  $('#formSignUp');
 
 //Stored Items
-  var favorites;
+  var favoritesList;
   var favoriteStoryIds;
   var auth_key = localStorage.getItem("auth_key");
   // var auth_key = false;
   var currentStory = localStorage.getItem("current_story") || 'top';
-
+  var $errorMsgLoc = $('.error');
 
   //On  page refresh validate auth_key by getting favorites then generate page
   (function (){
       if(auth_key === 'undefined' || !auth_key){
         auth_key = false
-        onRefresh()
+        displayStories()
         return
       }
 
       getData('favorites', 'stories', function(result){
         if(result.status > 299){
           auth_key = false
-          onRefresh()
+          displayStories()
         } else {
           getFavoriteStoryIds(result)
           $('.loggedIn').toggleClass('display-hide')
-          onRefresh()
+          displayStories()
         }
       })
   })();
 
-  function onRefresh(){
-    getData(currentStory, 'stories', function(stories){
+  function displayStories(type=currentStory){
+    getData(type, 'stories', function(stories){
       var selectStoryIds = stories.slice(lastStoryDisplayed, lastStoryDisplayed+numberGetStories)
 
       selectStoryIds.map(story =>
@@ -78,17 +79,23 @@
   }
 
 
-// Signout hide
+// Signout hide, rest
   $('#signOutBtn').click(function(){
+    var location = $('.glyphicon')
     $('.loggedIn').toggleClass('display-hide')
     localStorage.removeItem('auth_key')
     auth_key = false;
     favoriteStoryIds = {};
+
+    location.removeClass("glyphicon-star-empty")
+    location.removeClass("glyphicon-star")
   })
 
   // Hide/Show login form
   $navLoginBtn.click(function(){
+      $errorMsgLoc.text('')
       $mainLoginForm.toggleClass('display-hide')
+
   })
 
   //login sign up toggle between tabs
@@ -108,7 +115,7 @@
     postData('favorites', $loginSignup ,
       function(result){
           if(result.status > 299){
-            $('.error').text(errorMsgs.login)
+            $errorMsgLoc.text(errorMsgs.login)
           } else {
             localStorage.setItem("auth_key", result.auth_token)
             auth_key = result.auth_token
@@ -117,21 +124,97 @@
                 getFavoriteStoryIds(result)
                 $('.loggedIn').toggleClass('display-hide')
                 displayGliphicon('justLogged')
+                $errorMsgLoc.text('')
                 $mainLoginForm.toggleClass('display-hide')
             })
-
           }
       }
       ,$email.val(), $password.val())
 
+    //Reset form
     $email.val('')
     $password.val('')
   })
 
 
+  $navBtns.click(function(e){
+    currentStory = $(e.target).attr('id');
+
+    $dataDisplay.empty()
+
+    if(currentStory === 'favorites'){
+      favoritesList.forEach(story =>{
+        newData(story.title, story.url, story.created, story.by, story.story_id)
+      })
+
+    } else {
+      if(auth_key === 'undefined' || !auth_key){
+        auth_key = false
+        displayStories(currentStory)
+        return
+      }
+
+      getData('favorites', 'stories', function(result){
+        if(result.status > 299){
+          auth_key = false
+          displayStories(currentStory)
+          $('.loggedIn').toggleClass('display-hide')
+        } else {
+          getFavoriteStoryIds(result)
+          displayStories(currentStory)
+        }
+      })
+
+      localStorage.setItem("current_story", currentStory)
+    }
+
+
+  })
+
   //Select Favorite *STAR*
   $dataDisplay.on('click', '.glyphicon', function(e){
-    $(e.target).toggleClass('glyphicon-star glyphicon-star-empty')
+    var location = $(e.target);
+    var locationParent = location.parent().children();
+    var storyId = location.parent().attr('id').trim()
+
+    e.preventDefault();
+
+    if($(e.target).hasClass('glyphicon-star-empty')){
+       var addFav = {
+        hacker_news_story:{
+            by: locationParent.eq(3).children().text().trim(),
+            story_id: storyId,
+            title: locationParent.eq(1).text().trim(),
+            url: locationParent.eq(2).attr('href').trim()
+          }
+        }
+        postData('favorites', 'add', function(result){
+            if(result.status > 299){
+              $errorMsgLoc.text(errorMsgs.favorites)
+            } else {
+              location.toggleClass('glyphicon-star-empty glyphicon-star')
+              //Success full add does not return ID. Must refresh favorites to get id,
+                getData('favorites', 'stories', function(result){
+                if(result.status > 299){
+                  auth_key = false
+                } else {
+                  getFavoriteStoryIds(result)
+                }
+              })
+            }
+      }, addFav)
+    } else {
+        deleteData('favorites', 'delete', function(result){
+            if(result.status > 299){
+              $errorMsgLoc.text(errorMsgs.favorites)
+            } else {
+              location.toggleClass('glyphicon-star-empty glyphicon-star')
+              if(currentStory === 'favorites'){
+                $('#'+ storyId).remove()
+              }
+            }
+        }, favoriteStoryIds[location.parent().attr('id')])
+    }
   })
 
 
--- ajax_with_jquery_exercise/script_main_content.js
+++ ajax_with_jquery_exercise/script_main_content.js
@@ -2,8 +2,9 @@
 
 
 function getFavoriteStoryIds(stories){
+  favoritesList = stories
   favoriteStoryIds = stories.reduce((acc, story) => {
-    acc[story.story_id] = story.id
+    acc[story.story_id] = story.id;
     return acc
   },{})
 }
@@ -12,13 +13,14 @@ function newData(title, url, time, author, id){
   $dataDisplay.append(
     `<li id=${id}>
       ${displayGliphicon(id)}
-      ${title} ${createUrl(url)}
-      <p>submitted ${time} by ${author}</p>
+      <span>${title}</span> ${createUrl(url)}
+      <p>submitted ${time} by <span> ${author} </span></p>
     </li>
     `)
 }
 
 function createUrl(url){
+  url = url || 'http://rithmschool.com'
   var rmHttp= url.indexOf('//')+2;
   var urlShort = url.slice(rmHttp,url.indexOf('/', rmHttp) === -1 ? url.length: url.indexOf('/', rmHttp));
   return `<a href=" ${url}" target="_blank"> (${urlShort}) </a>`
@@ -27,11 +29,14 @@ function createUrl(url){
 function displayGliphicon(input){
 
     if(input === 'justLogged'){
-      for(key in favoriteStoryIds){
-
-        $('.glyphicon').removeClass('glyphicon-star-empty')
-        $('.glyphicon').addClass('glyphicon-star')
-      }
+      $('ol li').each(function(){
+        var location = $(this).children().eq(0)
+        if(favoriteStoryIds[$(this).attr('id')]){
+          location.addClass('glyphicon-star')
+        } else {
+          location.addClass('glyphicon-star-empty')
+        }
+      })
       return
     }
 
