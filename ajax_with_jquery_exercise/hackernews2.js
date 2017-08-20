62acbf09eda237477c3e7c61e9a41a89f22d9d18
Adele Landers <adelethefish@gmail.com>
2017-08-20 11:43:17 -0700

    progress on letting signed in user make a new favorite

--- ajax_with_jquery_exercise/hackernews2.js
+++ ajax_with_jquery_exercise/hackernews2.js
@@ -31,16 +31,41 @@ $(function(){
 		getLogin(username,password);
 	})
 
-	$items.on('click', '.glyphicon-star-empty, .glyphicon-star', function(e) {
-			$(e.target).toggleClass("glyphicon-star glyphicon-star-empty");
-	});
-
 	getTop();
 	//when logout is clicked, need to remove token
 
 //FUNCTIONS
+	var $star = 
 
-	function getLogin(username,password){
+	function addFavorites(authorization,storyId){
+		console.log(`this is inside addFavs function`)
+		$star.on('click', '.glyphicon-star-empty, .glyphicon-star', function(e,storyId) {
+			$(e.target).toggleClass("glyphicon-star glyphicon-star-empty");
+				if($star.hasClass('glyphicon-star') === true){
+					console.log(`${storyId} star is filled`)
+					//makeFavorites(storyId);
+				}
+			
+		});
+
+		$.ajax({
+			url: "https://hn-favorites.herokuapp.com/stories.json",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": authorization
+			}
+		})
+		.then(function(data) {
+			$items.empty();
+			//data is array of objects
+			for(var i = 0; i < data.length; i++){
+				displayLink(data[i].title, data[i].url, data[i].story_id)
+			}
+		})
+	}
+	
+	function getLogin(username,password,storyId){
 		$.ajax({
 		    method: "POST",
 		    headers: {
@@ -57,9 +82,12 @@ $(function(){
 			authorization = data.auth_token
 		})
 		.fail(err => console.warn(err))
+
+		addFavorites(authorization,storyId)
 	}
 
 	function displayLink(title="adele",url="http://wwww.google.com",storyId=0){
+		console.log(`this is inside displayLink function`)
 		var shortUrl = url.slice(12);
 		var $newLi = $("<li>", {
 			html: `${title} (<a href="${url}">${shortUrl}</a>)`,
@@ -71,13 +99,17 @@ $(function(){
 
 		$newLi.prepend($star);
 		$items.append($newLi);
+		getLogin(storyId)
+		
 	}
+
 	
 	//gets JSON data of top stories
 	
 
 	//takes in promises from stories or favorites (if logged in) and gets in format of title, url, id
 	function getTop(){
+		console.log(`this is inside getTOP function`)
 		$.getJSON('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
 	    .then(function(res) {
 	    	$items.empty();
@@ -98,6 +130,7 @@ $(function(){
 
 	function getFavorites(authorization){ 
 		//returns promise (object of data)
+		console.log(`this is inside getFavs function`)
 		$.ajax({
 			url: "https://hn-favorites.herokuapp.com/stories.json",
 			method: "GET",
@@ -114,7 +147,7 @@ $(function(){
 			}
 		})
 	}
-	
+
 });
 
 
